67adf3c33f82d0798bb90e44e87881075affcd86
ordogod <mmolosays@gmail.com>
2021-11-04 17:05:34 +0300

    Add progress view

--- app/src/main/java/com/ordolabs/thecolor/util/ext/PairExt.kt
+++ app/src/main/java/com/ordolabs/thecolor/util/ext/PairExt.kt
@@ -1,5 +1,13 @@
 package com.ordolabs.thecolor.util.ext
 
+infix fun <T> Pair<T, T>.order(firstToSecond: Boolean): Pair<T, T> {
+    return if (firstToSecond) this else Pair(second, first)
+}
+
 infix fun <T> Pair<T, T>.by(selector: Boolean): T {
     return if (selector) this.first else this.second
+}
+
+infix fun Pair<Float, Float>.by(factor: Float): Float {
+    return first + (second - first) * factor
 }
