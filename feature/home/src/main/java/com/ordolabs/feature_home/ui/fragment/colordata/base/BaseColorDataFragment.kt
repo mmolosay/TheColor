b97befa8a7140e034787fb4e95ce1bc5fa08caf0
ordogod <mmolosays@gmail.com>
2022-01-11 01:55:13 +0300

    KDoc edits

--- feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/base/BaseColorDataFragment.kt
+++ feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/base/BaseColorDataFragment.kt
@@ -3,7 +3,7 @@ package com.ordolabs.feature_home.ui.fragment.colordata.base
 import com.ordolabs.feature_home.ui.fragment.BaseFragment
 
 /**
- * Fragment that displays passed in arguments color details and nothing more.
+ * Fragment that displays color data, passed in arguments, and nothing more.
  * Has no functionality of independently obtaining data from outer source.
  */
 abstract class BaseColorDataFragment<D> :
