55e6a0d0bbe214b4c2f348b827c0c3cf9e2a4af8
ordogod <mmolosays@gmail.com>
2022-01-11 20:32:04 +0300

    Use real case exact color

--- feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/details/ColorDetailsFragment.kt
+++ feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/details/ColorDetailsFragment.kt
@@ -162,7 +162,7 @@ class ColorDetailsFragment :
             exactColor.backgroundTintList = ColorStateList.valueOf(color)
             exactLink.setOnClickListener {
                 exact ?: return@setOnClickListener
-                colorDetailsVM.getExactColor(/*exact*/ "#131344")
+                colorDetailsVM.getExactColor(exact)
             }
         }
 
