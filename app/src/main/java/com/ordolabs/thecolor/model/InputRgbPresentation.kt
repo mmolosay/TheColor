3a3d341f805fd15e702fb9fffba1340205c3b6ae
ordogod <mmolosays@gmail.com>
2022-01-11 21:24:45 +0300

    Add some KDoc

--- app/src/main/java/com/ordolabs/thecolor/model/InputRgbPresentation.kt
+++ app/src/main/java/com/ordolabs/thecolor/model/InputRgbPresentation.kt
@@ -3,6 +3,14 @@ package com.ordolabs.thecolor.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Presentation of RGB color, entered by user.
+ * May contain non-valid color or nothing at all.
+ *
+ * @param r R component in range (0-255) including both start and end.
+ * @param g G component in range (0-255) including both start and end.
+ * @param b B component in range (0-255) including both start and end.
+ */
 @Parcelize
 data class InputRgbPresentation(
     val r: Int?,
