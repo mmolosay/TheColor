b8343ffe3510f1190c320e7c02218057e2fe92bc
ordogod <mmolosays@gmail.com>
2022-01-15 15:11:36 +0300

    Inflate shimmer fragments with container ViewGroup theme

--- feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/details/ColorDetailsShimmerFragment.kt
+++ feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/details/ColorDetailsShimmerFragment.kt
@@ -1,9 +1,24 @@
 package com.ordolabs.feature_home.ui.fragment.colordata.details
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.ordolabs.feature_home.R
 import com.ordolabs.feature_home.ui.fragment.BaseFragment
+import com.ordolabs.thecolor.util.InflaterUtil.cloneInViewContext
 
-class ColorDetailsShimmerFragment : BaseFragment(R.layout.color_details_shimmer_fragment) {
+class ColorDetailsShimmerFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // inherit container view group theme
+        return inflater.cloneInViewContext(container)
+            .inflate(R.layout.color_details_shimmer_fragment, container, false)
+    }
 
     override fun collectViewModelsData() {
         // nothing is here
--- feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/scheme/ColorSchemeShimmerFragment.kt
+++ feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/scheme/ColorSchemeShimmerFragment.kt
@@ -1,9 +1,24 @@
 package com.ordolabs.feature_home.ui.fragment.colordata.scheme
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.ordolabs.feature_home.R
 import com.ordolabs.feature_home.ui.fragment.BaseFragment
+import com.ordolabs.thecolor.util.InflaterUtil.cloneInViewContext
 
-class ColorSchemeShimmerFragment : BaseFragment(R.layout.color_scheme_shimmer_fragment) {
+class ColorSchemeShimmerFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // inherit container view group theme
+        return inflater.cloneInViewContext(container)
+            .inflate(R.layout.color_scheme_shimmer_fragment, container, false)
+    }
 
     override fun collectViewModelsData() {
         // nothing is here
