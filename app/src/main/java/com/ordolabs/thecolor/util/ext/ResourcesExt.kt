f59c0fa6397c61f1291f3e9fe7294d5abe583ae0
ordogod <mmolosays@gmail.com>
2022-01-17 23:57:02 +0300

    Show start mode in title

--- app/src/main/java/com/ordolabs/thecolor/util/ext/ResourcesExt.kt
+++ app/src/main/java/com/ordolabs/thecolor/util/ext/ResourcesExt.kt
@@ -1,7 +1,16 @@
 package com.ordolabs.thecolor.util.ext
 
 import android.content.res.Resources
+import android.text.Annotation
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.SpannedString
+import androidx.annotation.StringRes
+import androidx.core.text.getSpans
 import com.ordolabs.thecolor.R
+import kotlin.math.min
+
+private const val STRING_RESOURCE_ANNOTATION_KEY = "ordinal"
 
 fun Resources.getStringYesOrNo(yes: Boolean): String {
     return if (yes) {
@@ -15,3 +24,33 @@ fun Resources.getStringOrNull(res: Int): String? =
     Result.runCatching {
         getString(res)
     }.getOrNull()
+
+/**
+ * Retrieves string from [Resources] by its [resid] and applies specified [spans] to it.
+ * String should containt `<annotation>` tag with [STRING_RESOURCE_ANNOTATION_KEY] as key
+ * and Integer as value.
+ * For example:
+ * ```
+ *      <string>This <annotation ordinal="0">is</annotation> a <annotation ordinal="1">string</annotation></string>
+ * ```
+ */
+fun Resources.getStringWithAnnotations(
+    @StringRes resid: Int,
+    spans: List<Any>
+): Spanned {
+    val spanned = this.getText(resid) as SpannedString
+    val annotations = spanned
+        .getSpans<Annotation>()
+        .filter { it.key == STRING_RESOURCE_ANNOTATION_KEY }
+        .sortedBy { it.value.toIntOrNull() }
+    val builder = SpannableStringBuilder(spanned)
+    val count = min(annotations.size, spans.size)
+    for (i in 0 until count) {
+        val span = spans[i]
+        val annotation = annotations[i]
+        val start = spanned.getSpanStart(annotation)
+        val end = spanned.getSpanEnd(annotation)
+        builder.setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+    return builder
+}
--- feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/scheme/editor/ColorSchemeConfigFragment.kt
+++ feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/colordata/scheme/editor/ColorSchemeConfigFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.buildSpannedString
+import androidx.core.text.color
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.chip.Chip
@@ -12,8 +14,10 @@ import com.ordolabs.feature_home.databinding.ColorSchemeConfigFragmentBinding
 import com.ordolabs.feature_home.ui.fragment.BaseFragment
 import com.ordolabs.feature_home.viewmodel.colordata.scheme.ColorSchemeConfigViewModel
 import com.ordolabs.thecolor.model.color.data.ColorScheme
+import com.ordolabs.thecolor.model.color.data.ColorSchemeRequest
 import com.ordolabs.thecolor.util.InflaterUtil.cloneInViewContext
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import com.google.android.material.R as RMaterial
 
 /**
  * Fragment that displays color scheme configuration options and passes them
@@ -34,6 +38,12 @@ class ColorSchemeConfigFragment : BaseFragment() {
         return inflater
             .cloneInViewContext(container)
             .inflate(R.layout.color_scheme_config_fragment, container, false)
+    }
+
+    override fun setUp() {
+        super.setUp()
+        val config = schemeConfigVM.appliedConfig
+        populateTitles(config)
     }
 
     override fun collectViewModelsData() {
@@ -68,6 +78,38 @@ class ColorSchemeConfigFragment : BaseFragment() {
         if (!isChecked) return // do nothing
         schemeConfigVM.mode = mode
     }
+
+    // region Populate
+
+    private fun populateTitles(config: ColorSchemeRequest.Config) {
+        val mode = enumValues<ColorScheme.Mode>()[config.modeOrdinal]
+        val sampleCount = config.sampleCount
+        populateModeTitle(mode)
+        populateSampleCountTitle(sampleCount)
+    }
+
+    private fun populateModeTitle(mode: ColorScheme.Mode) {
+        val titleView = binding.modeChipsTitle
+        val label = resources.getString(mode.labelRes)
+        val color = titleView.context
+            .getColorStateList(RMaterial.color.material_on_background_emphasis_medium)
+            .defaultColor
+        val base = resources.getString(R.string.color_scheme_config_mode_title)
+        val title = buildSpannedString {
+            append(base)
+            append(' ')
+            color(color) {
+                append(label)
+            }
+        }
+        titleView.text = title
+    }
+
+    private fun populateSampleCountTitle(sampleCount: Int) {
+        // TODO: implement
+    }
+
+    // endregion
 
     companion object {
         fun newInstance() =
