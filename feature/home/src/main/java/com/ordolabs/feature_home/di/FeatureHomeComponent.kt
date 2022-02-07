24f05838bb75dd28c1c4978c322fbb188567ac21
ordogod <mmolosays@gmail.com>
2022-02-08 01:05:29 +0300

    Implement body of 'FeatureHomeComponent'

--- feature/home/src/main/java/com/ordolabs/feature_home/di/FeatureHomeComponent.kt
+++ feature/home/src/main/java/com/ordolabs/feature_home/di/FeatureHomeComponent.kt
@@ -1,11 +1,23 @@
 package com.ordolabs.feature_home.di
 
 import com.ordolabs.feature_home.di.module.FeatureHomeModule
+import com.ordolabs.thecolor.di.AppComponent
 import com.ordolabs.thecolor.di.scope.FeatureScope
 import dagger.Component
 
+// TODO: build in HomeFragment
 @FeatureScope
 @Component(
-    modules = [FeatureHomeModule::class]
+    modules = [FeatureHomeModule::class],
+    dependencies = [AppComponent::class]
 )
-interface FeatureHomeComponent
+interface FeatureHomeComponent {
+
+    @Component.Builder
+    interface Builder {
+
+        fun appComponent(appComponent: AppComponent): Builder
+
+        fun build(): FeatureHomeComponent
+    }
+}
