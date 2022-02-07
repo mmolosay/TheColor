1d78a8b6515264531fbc799a4743c63ab9fafd72
ordogod <mmolosays@gmail.com>
2022-02-08 00:50:38 +0300

    Get rid of unused labels

--- domain/src/main/java/com/ordolabs/domain/di/module/DomainUseCaseModule.kt
+++ domain/src/main/java/com/ordolabs/domain/di/module/DomainUseCaseModule.kt
@@ -44,13 +44,4 @@ class DomainUseCaseModule {
         repository: IColorRemoteRepository
     ): GetColorSchemeBaseUseCase =
         GetColorSchemeUseCase(repository)
-
-    companion object {
-        // TODO: get rid of?
-        const val VALIDATE_COLOR_HEX_USE_CASE = "validate_color_hex_use_case"
-        const val VALIDATE_COLOR_RGB_USE_CASE = "validate_color_rgb_use_case"
-
-        const val GET_COLOR_DETAILS_USE_CASE = "get_color_details_use_case"
-        const val GET_COLOR_SCHEME_USE_CASE = "get_color_scheme_use_case"
-    }
 }
