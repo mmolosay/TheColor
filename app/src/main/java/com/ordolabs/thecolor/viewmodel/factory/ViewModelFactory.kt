d836944c5d67c93dea08c59e535fa48e6eac58c0
ordogod <mmolosays@gmail.com>
2022-02-10 19:56:57 +0300

    Rename

--- app/src/main/java/com/ordolabs/thecolor/viewmodel/factory/ViewModelFactory.kt
+++ app/src/main/java/com/ordolabs/thecolor/viewmodel/factory/ViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 import javax.inject.Provider
 
-class ViewModelsFactory @Inject constructor(
+class ViewModelFactory @Inject constructor(
     private val factories: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
 
