262f5007ac668ec363aeda3c0ee9989c4c781695
ordogod <mmolosays@gmail.com>
2022-02-10 19:56:44 +0300

    Intorduce 'ViewModelsFactory'

---
+++ app/src/main/java/com/ordolabs/thecolor/viewmodel/factory/ViewModelsFactory.kt
@@ -0,0 +1,21 @@
+package com.ordolabs.thecolor.viewmodel.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+
+class ViewModelsFactory @Inject constructor(
+    private val factories: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+
+    val viewModelsClasses: Set<Class<out ViewModel>>
+        get() = factories.keys
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val key = modelClass as Class<ViewModel>
+        val provider = factories.getValue(key)
+        return provider.get() as T
+    }
+}
