7ae87fe461683b1d8247c0254787e78f9b2cdd2c
ordogod <mmolosays@gmail.com>
2022-02-11 12:16:06 +0300

    Rename

--- app/src/main/java/com/ordolabs/thecolor/viewmodel/factory/ViewModelFactory.kt
+++ app/src/main/java/com/ordolabs/thecolor/viewmodel/factory/ViewModelFactory.kt
@@ -6,13 +6,13 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor(
-    private val factories: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+    private val providers: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val key = modelClass as Class<ViewModel>
-        val provider = factories.getValue(key)
+        val provider = providers.getValue(key)
         return provider.get() as T
     }
 }
