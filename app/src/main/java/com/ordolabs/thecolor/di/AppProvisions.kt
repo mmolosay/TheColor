e3c31ebd17df750c510f5a9895ca2f0c04bfb841
ordogod <mmolosays@gmail.com>
2022-02-11 12:24:22 +0300

    Make 'ViewModelFactory' instance be unique in each scope

--- app/src/main/java/com/ordolabs/thecolor/di/AppProvisions.kt
+++ app/src/main/java/com/ordolabs/thecolor/di/AppProvisions.kt
@@ -1,12 +1,10 @@
 package com.ordolabs.thecolor.di
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.ordolabs.domain.di.DomainProvisions
 
 interface AppProvisions :
     DomainProvisions {
 
     val viewModelMultibinding: Map<Class<out ViewModel>, @JvmSuppressWildcards ViewModel>
-    val viewModelFactory: ViewModelProvider.Factory
 }
--- app/src/main/java/com/ordolabs/thecolor/di/module/AppViewModelModule.kt
+++ app/src/main/java/com/ordolabs/thecolor/di/module/AppViewModelModule.kt
@@ -1,9 +1,6 @@
 package com.ordolabs.thecolor.di.module
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.ordolabs.thecolor.viewmodel.factory.ViewModelFactory
-import dagger.Binds
 import dagger.Module
 import dagger.multibindings.Multibinds
 
@@ -12,7 +9,4 @@ interface AppViewModelModule {
 
     @Multibinds
     fun multibindViewModels(): Map<Class<out ViewModel>, @JvmSuppressWildcards ViewModel>
-
-    @Binds
-    fun bindViewModelFactory(instance: ViewModelFactory): ViewModelProvider.Factory
 }
