dac76b08c11c95ec606e2a736b475616a984c474
ordogod <mmolosays@gmail.com>
2022-02-15 00:15:28 +0300

    Add TODO

--- app/src/main/java/com/ordolabs/thecolor/util/ext/StringExt.kt
+++ app/src/main/java/com/ordolabs/thecolor/util/ext/StringExt.kt
@@ -2,5 +2,6 @@ package com.ordolabs.thecolor.util.ext
 
 import androidx.fragment.app.Fragment
 
+// TODO: abolish; should be applied to SharedPreferences keys, and not required in Bundles
 inline fun <reified F : Fragment> String.makeArgumentsKey(): String =
     "${F::class.java.canonicalName}.$this"
