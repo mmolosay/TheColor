4c70c370fdccda58577b7754cc5e2e18310dfd60
ordogod <mmolosays@gmail.com>
2022-02-26 01:31:41 +0300

    Add utils for make checking specific state simpler

--- feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/home/HomeView.kt
+++ feature/home/src/main/java/com/ordolabs/feature_home/ui/fragment/home/HomeView.kt
@@ -9,16 +9,33 @@ interface HomeView {
         /**
          * Has neither color preview or color data shown.
          */
-        BLANK,
+        BLANK {
+            override fun isBlank(): Boolean = true
+            override fun isPreview(): Boolean = false
+            override fun isData(): Boolean = false
+        },
 
         /**
          * Has color preview shown only.
          */
-        PREVIEW,
+        PREVIEW {
+            override fun isBlank(): Boolean = false
+            override fun isPreview(): Boolean = true
+            override fun isData(): Boolean = false
+        },
 
         /**
          * Has both color preview and color data shown.
          */
-        DATA
+        DATA {
+            override fun isBlank(): Boolean = false
+            override fun isPreview(): Boolean = false
+            override fun isData(): Boolean = true
+        };
+
+        // utils to make checking specific state easier
+        abstract fun isBlank(): Boolean
+        abstract fun isPreview(): Boolean
+        abstract fun isData(): Boolean
     }
 }
