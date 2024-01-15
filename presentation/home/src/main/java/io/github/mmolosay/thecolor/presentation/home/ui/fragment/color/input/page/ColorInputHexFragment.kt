fa9d87a6ac60debd312c7cd4a30b62bd75b91c83
mmolosay <mmolosays@gmail.com>
2024-01-15 23:57:46 +0100

    Replace old View with new Compose

--- presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/ui/fragment/color/input/page/ColorInputHexFragment.kt
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/ui/fragment/color/input/page/ColorInputHexFragment.kt
@@ -4,67 +4,64 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.doOnTextChanged
-import by.kirich1409.viewbindingdelegate.viewBinding
-import io.github.mmolosay.thecolor.presentation.home.R
-import io.github.mmolosay.thecolor.presentation.home.databinding.ColorInputHexFragmentBinding
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.viewModels
 import io.github.mmolosay.thecolor.presentation.color.ColorPrototype
 import io.github.mmolosay.thecolor.presentation.home.input.ColorInputHex
-import io.github.mmolosay.thecolor.presentation.util.ext.getText
-import io.github.mmolosay.thecolor.presentation.util.ext.getTextString
-import io.github.mmolosay.thecolor.presentation.util.ext.setTextPreservingSelection
+import io.github.mmolosay.thecolor.presentation.home.input.ColorInputViewModel
 import io.github.mmolosay.thecolor.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
 class ColorInputHexFragment :
     BaseColorInputFragment<ColorPrototype.Hex>() {
 
-    private val binding by viewBinding(ColorInputHexFragmentBinding::bind)
+//    private val binding by viewBinding(ColorInputHexFragmentBinding::bind)
+
+    private val vm: ColorInputViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.color_input_hex_fragment, container, false)
-    }
+        savedInstanceState: Bundle?,
+    ): View =
+        ComposeView(inflater.context).apply {
+            setContent {
+                ColorInputHex(vm = vm)
+            }
+        }
+
 
     // region Set views
 
     override fun setViews() {
-        setInputTextWatcher()
-        setComposeView()
+//        setInputTextWatcher()
     }
 
-    private fun setInputTextWatcher() =
-        binding.inputHex.editText?.doOnTextChanged { _, _, _, _ ->
-            outputOnInputChanges()
-        }
-
-    private fun setComposeView() {
-        binding.composeView.setContent {
-            ColorInputHex()
-        }
-    }
+//    private fun setInputTextWatcher() =
+//        binding.inputHex.editText?.doOnTextChanged { _, _, _, _ ->
+//            outputOnInputChanges()
+//        }
 
     // endregion
 
     // region BaseColorInputFragment
 
     override fun assemblePrototype(): ColorPrototype.Hex {
-        val input = binding.inputHex.getTextString()
+        val input = vm.uiDataFlow.value.input
         return ColorPrototype.Hex(value = input)
     }
 
     override fun populateViews(color: ColorPrototype.Hex): Unit =
-        binding.run {
-            inputHex.editText?.setTextPreservingSelection(color.value)
-        }
+        Unit
+//        binding.run {
+//            inputHex.editText?.setTextPreservingSelection(color.value) // TODO: resolve
+//        }
 
     override fun clearViews(): Unit =
-        binding.run {
-            inputHex.getText()?.clear()
-        }
+        Unit
+//        binding.run {
+//            inputHex.getText()?.clear() TODO: resolve
+//        }
 
     override fun getColorInputFlow(): Flow<Resource<ColorPrototype.Hex>> =
         colorInputVM.inputHex
