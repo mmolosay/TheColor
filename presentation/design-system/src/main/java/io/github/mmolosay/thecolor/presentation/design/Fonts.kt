f3ffc1de4d4b4bb183809d0633db0dd6290d5533
mmolosay <mmolosays@gmail.com>
2024-01-15 22:31:54 +0100

    Add file for future font

---
+++ presentation/design-system/src/main/java/io/github/mmolosay/thecolor/presentation/design/Fonts.kt
@@ -0,0 +1,8 @@
+package io.github.mmolosay.thecolor.presentation.design
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+
+//val nunitoFamily = FontFamily(
+//    Font()
+//)
