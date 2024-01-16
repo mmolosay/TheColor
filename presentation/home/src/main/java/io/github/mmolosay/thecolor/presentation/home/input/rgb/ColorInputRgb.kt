00081655a52506adacbc45bb9326750f8019050d
mmolosay <mmolosays@gmail.com>
2024-01-16 21:20:46 +0100

    Implement 'ColorInputRgb' UI component

---
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/input/rgb/ColorInputRgb.kt
@@ -0,0 +1,89 @@
+package io.github.mmolosay.thecolor.presentation.home.input.rgb
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.mmolosay.thecolor.presentation.design.TheColorTheme
+import io.github.mmolosay.thecolor.presentation.home.input.ColorInputComponents.TextField
+import io.github.mmolosay.thecolor.presentation.home.input.ColorInputFieldUiData
+
+@Composable
+internal fun ColorInputRgb(
+    uiData: ColorInputRgbUiData,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        TextField(uiData.rInputField)
+        TextField(uiData.gInputField)
+        TextField(uiData.bInputField)
+    }
+}
+
+@Composable
+private fun TextField(
+    uiData: ColorInputFieldUiData,
+) {
+    var value by remember { mutableStateOf(TextFieldValue(text = uiData.text)) }
+    TextField(
+        uiData = uiData,
+        value = value,
+        updateValue = { new -> value = new },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    TheColorTheme {
+        ColorInputRgb(
+            uiData = previewUiData(),
+        )
+    }
+}
+
+private fun previewUiData() =
+    ColorInputRgbUiData(
+        rInputField = ColorInputFieldUiData(
+            text = "12",
+            onTextChange = {},
+            processText = { it },
+            label = "R",
+            placeholder = "0",
+            prefix = "#",
+            trailingButton = ColorInputFieldUiData.TrailingButton.Visible(
+                onClick = {},
+                iconContentDesc = "",
+            ),
+        ),
+        gInputField = ColorInputFieldUiData(
+            text = "",
+            onTextChange = {},
+            processText = { it },
+            label = "G",
+            placeholder = "0",
+            prefix = "#",
+            trailingButton = ColorInputFieldUiData.TrailingButton.Visible(
+                onClick = {},
+                iconContentDesc = "",
+            ),
+        ),
+        bInputField = ColorInputFieldUiData(
+            text = "255",
+            onTextChange = {},
+            processText = { it },
+            label = "B",
+            placeholder = "0",
+            prefix = "#",
+            trailingButton = ColorInputFieldUiData.TrailingButton.Visible(
+                onClick = {},
+                iconContentDesc = "",
+            ),
+        ),
+    )
---
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/input/rgb/ColorInputRgbUiData.kt
@@ -0,0 +1,9 @@
+package io.github.mmolosay.thecolor.presentation.home.input.rgb
+
+import io.github.mmolosay.thecolor.presentation.home.input.ColorInputFieldUiData
+
+data class ColorInputRgbUiData(
+    val rInputField: ColorInputFieldUiData,
+    val gInputField: ColorInputFieldUiData,
+    val bInputField: ColorInputFieldUiData,
+)
