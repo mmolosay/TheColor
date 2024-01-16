15b0fabe7fd8d3007b623781936930b69b5a1d06
mmolosay <mmolosays@gmail.com>
2024-01-16 20:22:20 +0100

    Extract 'OutlinedTextField' into reusable component

---
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/input/ColorInputComponents.kt
@@ -0,0 +1,105 @@
+package io.github.mmolosay.thecolor.presentation.home.input
+
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+
+/**
+ * Reusable UI components of color input widgets.
+ */
+internal object ColorInputComponents {
+
+    @Composable
+    fun TextField(
+        modifier: Modifier = Modifier,
+        uiData: ColorInputFieldUiData,
+        value: TextFieldValue,
+        updateValue: (TextFieldValue) -> Unit,
+    ) {
+        OutlinedTextField(
+            modifier = modifier
+                .selectAllTextOnFocus(
+                    value = value,
+                    onValueChange = updateValue,
+                ),
+            value = value,
+            onValueChange = { new ->
+                updateValue(new)
+                uiData.onTextChange(new.text)
+            },
+            label = { Label(uiData.label) },
+            placeholder = { Placeholder(uiData.placeholder) },
+            trailingIcon = { TrailingButton(uiData.trailingButton) },
+            prefix = { Prefix(uiData.prefix) },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(),
+            singleLine = true,
+        )
+        LaunchedEffect(uiData.text) {
+            val new = value.copy(text = uiData.text)
+            updateValue(new)
+        }
+    }
+
+    @Composable
+    private fun Label(text: String) =
+        Text(
+            text = text,
+        )
+
+    @Composable
+    private fun Placeholder(text: String) =
+        Text(
+            text = text,
+        )
+
+    @Composable
+    private fun TrailingButton(uiData: ColorInputFieldUiData.TrailingButton) =
+        when (uiData) {
+            is ColorInputFieldUiData.TrailingButton.Visible -> ClearIconButton(uiData)
+            is ColorInputFieldUiData.TrailingButton.Hidden -> Unit // don't show anything
+        }
+
+    @Composable
+    private fun ClearIconButton(uiData: ColorInputFieldUiData.TrailingButton.Visible) {
+        IconButton(
+            onClick = uiData.onClick,
+        ) {
+            Icon(
+                imageVector = Icons.Default.Clear,
+                contentDescription = uiData.iconContentDesc,
+            )
+        }
+    }
+
+    @Composable
+    private fun Prefix(text: String) =
+        Text(
+            text = text,
+        )
+
+    private fun Modifier.selectAllTextOnFocus(
+        value: TextFieldValue,
+        onValueChange: (TextFieldValue) -> Unit,
+    ) =
+        onFocusChanged a@{
+            if (!it.isFocused) return@a
+            val text = value.text
+            val newValue = value.copy(
+                selection = TextRange(start = 0, end = text.length)
+            )
+            onValueChange(newValue)
+        }
+}
--- presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/input/hex/ColorInputHex.kt
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/input/hex/ColorInputHex.kt
@@ -1,30 +1,18 @@
 package io.github.mmolosay.thecolor.presentation.home.input.hex
 
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.github.mmolosay.thecolor.presentation.design.TheColorTheme
 import io.github.mmolosay.thecolor.presentation.home.R
+import io.github.mmolosay.thecolor.presentation.home.input.ColorInputComponents.TextField
 import io.github.mmolosay.thecolor.presentation.home.input.ColorInputFieldUiData
 import io.github.mmolosay.thecolor.presentation.home.input.ColorInputFieldUiData.TrailingButton
 import io.github.mmolosay.thecolor.presentation.home.input.plus
@@ -44,83 +32,15 @@ internal fun ColorInputHex(
 
 @Composable
 internal fun ColorInputHex(
-    modifier: Modifier = Modifier,
     uiData: ColorInputHexUiData,
 ) {
     var value by remember { mutableStateOf(TextFieldValue(text = uiData.inputField.text)) }
-    OutlinedTextField(
-        modifier = modifier
-            .selectAllTextOnFocus(
-                value = value,
-                onValueChange = { new -> value = new },
-            ),
+    TextField(
+        uiData = uiData.inputField,
         value = value,
-        onValueChange = { new ->
-            value = new
-            uiData.inputField.onTextChange(new.text)
-        },
-        label = { Label(uiData.inputField.label) },
-        placeholder = { Placeholder(uiData.inputField.placeholder) },
-        trailingIcon = { TrailingButton(uiData.inputField.trailingButton) },
-        prefix = { Prefix(uiData.inputField.prefix) },
-        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-        keyboardActions = KeyboardActions(),
-        singleLine = true,
+        updateValue = { new -> value = new },
     )
-    LaunchedEffect(uiData.inputField.text) {
-        value = value.copy(text = uiData.inputField.text)
-    }
 }
-
-@Composable
-private fun Label(text: String) =
-    Text(
-        text = text,
-    )
-
-@Composable
-private fun Placeholder(text: String) =
-    Text(
-        text = text,
-    )
-
-@Composable
-private fun TrailingButton(uiData: TrailingButton) =
-    when (uiData) {
-        is TrailingButton.Visible -> ClearIconButton(uiData)
-        is TrailingButton.Hidden -> Unit // don't show anything
-    }
-
-@Composable
-private fun ClearIconButton(uiData: TrailingButton.Visible) {
-    IconButton(
-        onClick = uiData.onClick,
-    ) {
-        Icon(
-            imageVector = Icons.Default.Clear,
-            contentDescription = uiData.iconContentDesc,
-        )
-    }
-}
-
-@Composable
-private fun Prefix(text: String) =
-    Text(
-        text = text,
-    )
-
-private fun Modifier.selectAllTextOnFocus(
-    value: TextFieldValue,
-    onValueChange: (TextFieldValue) -> Unit,
-) =
-    onFocusChanged a@{
-        if (!it.isFocused) return@a
-        val text = value.text
-        val newValue = value.copy(
-            selection = TextRange(start = 0, end = text.length)
-        )
-        onValueChange(newValue)
-    }
 
 @Composable
 private fun rememberViewData() =
