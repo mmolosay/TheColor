fdf520d3e3db710d8bae202baa3528b3606e8e42
mmolosay <mmolosays@gmail.com>
2024-01-18 00:11:30 +0100

    Add methods of assembling 'ColorPrototype's out of UI data

--- presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/input/hex/ColorInputHexUiData.kt
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/input/hex/ColorInputHexUiData.kt
@@ -1,7 +1,11 @@
 package io.github.mmolosay.thecolor.presentation.home.input.hex
 
+import io.github.mmolosay.thecolor.presentation.color.ColorPrototype
 import io.github.mmolosay.thecolor.presentation.home.input.field.ColorInputFieldUiData
 
 data class ColorInputHexUiData(
     val inputField: ColorInputFieldUiData,
 )
+
+fun ColorInputHexUiData.assembleColorPrototype() =
+    ColorPrototype.Hex(value = inputField.text)
--- presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/input/rgb/ColorInputRgbUiData.kt
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/input/rgb/ColorInputRgbUiData.kt
@@ -1,5 +1,6 @@
 package io.github.mmolosay.thecolor.presentation.home.input.rgb
 
+import io.github.mmolosay.thecolor.presentation.color.ColorPrototype
 import io.github.mmolosay.thecolor.presentation.home.input.field.ColorInputFieldUiData
 
 data class ColorInputRgbUiData(
@@ -7,3 +8,10 @@ data class ColorInputRgbUiData(
     val gInputField: ColorInputFieldUiData,
     val bInputField: ColorInputFieldUiData,
 )
+
+fun ColorInputRgbUiData.assembleColorPrototype() =
+    ColorPrototype.Rgb(
+        r = rInputField.text.toIntOrNull(),
+        g = gInputField.text.toIntOrNull(),
+        b = bInputField.text.toIntOrNull(),
+    )
