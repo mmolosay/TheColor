1ca79edf46c8c214c6b2dbdf6a511ad921c37e48
mmolosay <mmolosays@gmail.com>
2024-01-17 20:03:01 +0100

    Add Preview for text styles

--- presentation/design-system/src/main/java/io/github/mmolosay/thecolor/presentation/design/Typography.kt
+++ presentation/design-system/src/main/java/io/github/mmolosay/thecolor/presentation/design/Typography.kt
@@ -1,9 +1,12 @@
 package io.github.mmolosay.thecolor.presentation.design
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
 
 val LocalFontFamily = staticCompositionLocalOf { nunitoFamily }
 
@@ -32,3 +35,74 @@ internal fun typography() =
             labelSmall = labelSmall.copy(fontFamily = localFontFamily),
         )
     }
+
+@Preview(showBackground = true)
+@Composable
+private fun TypographyPreview() {
+    TheColorTheme {
+        Column {
+            fun textForStyle(styleName: String) = "Preview of $styleName."
+            val typography = MaterialTheme.typography
+            Text(
+                text = textForStyle("displayLarge"),
+                style = typography.displayLarge,
+            )
+            Text(
+                text = textForStyle("displayMedium"),
+                style = typography.displayMedium,
+            )
+            Text(
+                text = textForStyle("displaySmall"),
+                style = typography.displaySmall,
+            )
+            Text(
+                text = textForStyle("headlineLarge"),
+                style = typography.headlineLarge,
+            )
+            Text(
+                text = textForStyle("headlineMedium"),
+                style = typography.headlineMedium,
+            )
+            Text(
+                text = textForStyle("headlineSmall"),
+                style = typography.headlineSmall,
+            )
+            Text(
+                text = textForStyle("titleLarge"),
+                style = typography.titleLarge,
+            )
+            Text(
+                text = textForStyle("titleMedium"),
+                style = typography.titleMedium,
+            )
+            Text(
+                text = textForStyle("titleSmall"),
+                style = typography.titleSmall,
+            )
+            Text(
+                text = textForStyle("bodyLarge"),
+                style = typography.bodyLarge,
+            )
+            Text(
+                text = textForStyle("bodyMedium"),
+                style = typography.bodyMedium,
+            )
+            Text(
+                text = textForStyle("bodySmall"),
+                style = typography.bodySmall,
+            )
+            Text(
+                text = textForStyle("labelLarge"),
+                style = typography.labelLarge,
+            )
+            Text(
+                text = textForStyle("labelMedium"),
+                style = typography.labelMedium,
+            )
+            Text(
+                text = textForStyle("labelSmall"),
+                style = typography.labelSmall,
+            )
+        }
+    }
+}
