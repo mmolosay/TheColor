8e2d94ee510a1d3024374f85d9ef553df8f98b07
mmolosay <mmolosays@gmail.com>
2024-01-17 16:34:32 +0100

    Add dependencies for unit testing

--- presentation/home/build.gradle.kts
+++ presentation/home/build.gradle.kts
@@ -41,6 +41,7 @@ android {
     }
 }
 
+@Suppress("SpellCheckingInspection")
 dependencies {
     // Modules
     implementation(project(":domain"))
@@ -92,4 +93,10 @@ dependencies {
     // Hilt
     implementation("com.google.dagger:hilt-android:${libs.versions.hilt.get()}")
     kapt("com.google.dagger:hilt-compiler:${libs.versions.hilt.get()}")
+
+    // Testing
+    testImplementation("junit:junit:${libs.versions.junit.get()}")
+    testImplementation("io.mockk:mockk:${libs.versions.mockk.get()}")
+    testImplementation("io.kotest:kotest-assertions-core:${libs.versions.kotestAssertions.get()}")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${libs.versions.coroutines.get()}")
 }
