b3708f486a09e1d6fcaacff51b369e807d1b34e4
mmolosay <mmolosays@gmail.com>
2024-01-18 00:24:52 +0100

    Tweak converting to domain model to avoid having colors in short form (like "#123" instead of "#112233")

--- presentation/common/src/main/java/io/github/mmolosay/thecolor/presentation/mapper/PresentationToDomain.kt
+++ presentation/common/src/main/java/io/github/mmolosay/thecolor/presentation/mapper/PresentationToDomain.kt
@@ -5,24 +5,25 @@ import io.github.mmolosay.thecolor.presentation.color.ColorPrototype
 import io.github.mmolosay.thecolor.presentation.color.data.ColorSchemeRequest
 import io.github.mmolosay.thecolor.presentation.color.from
 import io.github.mmolosay.thecolor.presentation.color.toHex
+import io.github.mmolosay.thecolor.presentation.color.toRgb
 import io.github.mmolosay.thecolor.domain.model.Color.Hex as ColorHexDomain
 import io.github.mmolosay.thecolor.domain.model.Color.Rgb as ColorRgbDomain
 import io.github.mmolosay.thecolor.domain.model.ColorSchemeRequest as ColorSchemeRequestDomain
 
 // TODO: replace with some ColorFactory that turns presentation ColorPrototype-s into domain Color-s
 fun ColorPrototype.Hex.toDomainOrNull(): ColorHexDomain? {
-    Color.from(this) ?: return null
+    val color = Color.from(this)?.toHex() ?: return null
     return ColorHexDomain(
-        value = this.value!!, // checked in converting to valid color above
+        value = color.value!!, // checked in converting to valid color above
     )
 }
 
 fun ColorPrototype.Rgb.toDomainOrNull(): ColorRgbDomain? {
-    Color.from(this) ?: return null
+    val color = Color.from(this)?.toRgb() ?: return null
     return ColorRgbDomain(
-        r = this.r!!, // checked in converting to valid color above
-        g = this.g!!, // checked in converting to valid color above
-        b = this.b!!, // checked in converting to valid color above
+        r = color.r!!, // checked in converting to valid color above
+        g = color.g!!, // checked in converting to valid color above
+        b = color.b!!, // checked in converting to valid color above
     )
 }
 
