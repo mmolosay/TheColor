0b50c8060f4ea8071488ac647c25bd96babc5824
mmolosay <mmolosays@gmail.com>
2024-01-19 20:28:50 +0100

    Simplify implementations due to recent change to 'Color' derivatives

--- domain/src/main/java/io/github/mmolosay/thecolor/domain/usecase/ValidateColorHexUseCase.kt
+++ domain/src/main/java/io/github/mmolosay/thecolor/domain/usecase/ValidateColorHexUseCase.kt
@@ -3,6 +3,7 @@ package io.github.mmolosay.thecolor.domain.usecase
 import io.github.mmolosay.thecolor.domain.model.Color
 import javax.inject.Inject
 
+// TODO: delete; now Color.Hex is valid by definition
 class ValidateColorHexUseCase @Inject constructor() {
 
     private val hexColorValidationRegex: Regex by lazy {
@@ -11,6 +12,7 @@ class ValidateColorHexUseCase @Inject constructor() {
 
     operator fun invoke(color: Color.Hex?): Boolean {
         color ?: return false
-        return hexColorValidationRegex.matches(color.value)
+//        return hexColorValidationRegex.matches(color.value)
+        return true
     }
 }
--- domain/src/main/java/io/github/mmolosay/thecolor/domain/usecase/ValidateColorRgbUseCase.kt
+++ domain/src/main/java/io/github/mmolosay/thecolor/domain/usecase/ValidateColorRgbUseCase.kt
@@ -3,13 +3,15 @@ package io.github.mmolosay.thecolor.domain.usecase
 import io.github.mmolosay.thecolor.domain.model.Color
 import javax.inject.Inject
 
+// TODO: delete; now Color.Rgb is valid by definition
 class ValidateColorRgbUseCase @Inject constructor() {
 
     private val componentRange by lazy { 0..255 }
 
     operator fun invoke(color: Color.Rgb?): Boolean {
         color ?: return false
-        val components = listOf(color.r, color.b, color.g)
-        return components.all { it in componentRange }
+//        val components = listOf(color.r, color.b, color.g)
+//        return components.all { it in componentRange }
+        return true
     }
 }
