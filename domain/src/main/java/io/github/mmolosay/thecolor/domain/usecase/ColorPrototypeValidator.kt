c14e3ac6dd1a536e67b0940b0e34e5f5b2f7a2f6
mmolosay <mmolosays@gmail.com>
2024-01-19 19:11:41 +0100

    Introduce 'ColorPrototype' in domain layer and a use case to validate it

---
+++ domain/src/main/java/io/github/mmolosay/thecolor/domain/usecase/ColorPrototypeValidator.kt
@@ -0,0 +1,28 @@
+package io.github.mmolosay.thecolor.domain.usecase
+
+import io.github.mmolosay.thecolor.domain.model.ColorPrototype
+import javax.inject.Inject
+
+/**
+ * Checks whether provided [ColorPrototype] is a valid color or not.
+ */
+class ColorPrototypeValidator @Inject constructor() {
+
+    fun ColorPrototype.isValid(): Boolean =
+        when (this) {
+            is ColorPrototype.Hex -> this.isValid()
+            is ColorPrototype.Rgb -> this.isValid()
+        }
+
+    fun ColorPrototype.Hex.isValid(): Boolean {
+        value ?: return false
+        fun inRange() = value in 0x000000..0xFFFFFF
+        return inRange()
+    }
+
+    fun ColorPrototype.Rgb.isValid(): Boolean {
+        if ((r == null || g == null || b == null)) return false
+        fun Int.isInComponentRange() = this in 0..255
+        return (r.isInComponentRange() && g.isInComponentRange() && b.isInComponentRange())
+    }
+}
