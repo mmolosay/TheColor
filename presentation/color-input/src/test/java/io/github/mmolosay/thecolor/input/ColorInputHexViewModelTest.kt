16211d64965106cf77486a883b1da8238d76f0b5
mmolosay <mmolosays@gmail.com>
2024-01-22 15:21:52 +0100

    Update unit tests

--- presentation/color-input/src/test/java/io/github/mmolosay/thecolor/input/ColorInputHexViewModelTest.kt
+++ presentation/color-input/src/test/java/io/github/mmolosay/thecolor/input/ColorInputHexViewModelTest.kt
@@ -6,8 +6,11 @@ import io.github.mmolosay.thecolor.input.field.TextFieldUiData.ViewData.Trailing
 import io.github.mmolosay.thecolor.input.hex.ColorInputHexUiData
 import io.github.mmolosay.thecolor.input.hex.ColorInputHexViewModel
 import io.github.mmolosay.thecolor.input.model.ColorInput
+import io.github.mmolosay.thecolor.input.model.UiState
 import io.github.mmolosay.thecolor.testing.MainDispatcherRule
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.beOfType
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
@@ -17,6 +20,7 @@ import io.mockk.runs
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
@@ -32,17 +36,51 @@ class ColorInputHexViewModelTest {
     }
 
     val mediator: ColorInputMediator = mockk {
-        every { hexColorInputFlow } returns emptyFlow()
+        every { hexColorInputFlow } returns flowOf(ColorInput.Hex(""))
         coEvery { send(any()) } just runs
     }
 
     lateinit var sut: ColorInputHexViewModel
 
-    val uiData: ColorInputHexUiData
+    val uiState: UiState<ColorInputHexUiData>
         get() = sut.uiStateFlow.value
 
+    val uiData: ColorInputHexUiData
+        get() {
+            uiState should beOfType<UiState.Ready<*>>() // assertion for clear failure message
+            return (uiState as UiState.Ready).uiData
+        }
+
     @Test
-    fun `filtering keeps only digits and letters A-F`() = runTest {
+    fun `sut is created with UiState BeingInitialized if mediator HEX flow has no value yet`() {
+        every { mediator.hexColorInputFlow } returns emptyFlow()
+
+        createSut()
+
+        uiState should beOfType<UiState.BeingInitialized>()
+    }
+
+    @Test
+    fun `sut is created with UiState Ready if mediator HEX flow has value already`() {
+        createSut()
+
+        uiState should beOfType<UiState.Ready<*>>()
+    }
+
+    @Test
+    fun `UiState becomes Ready when mediator emits first value from HEX flow`() =
+        runTest(mainDispatcherRule.testDispatcher) {
+            val hexColorInputFlow = MutableSharedFlow<ColorInput.Hex>()
+            every { mediator.hexColorInputFlow } returns hexColorInputFlow
+            createSut()
+
+            hexColorInputFlow.emit(ColorInput.Hex(""))
+
+            uiState should beOfType<UiState.Ready<*>>()
+        }
+
+    @Test
+    fun `filtering keeps only digits and letters A-F`() {
         createSut()
 
         val result = uiData.textField.filterUserInput("123abc_!.@ABG")
@@ -51,7 +89,7 @@ class ColorInputHexViewModelTest {
     }
 
     @Test
-    fun `filtering keeps only first 6 characters`() = runTest {
+    fun `filtering keeps only first 6 characters`() {
         createSut()
 
         val result = uiData.textField.filterUserInput("123456789ABCDEF")
