6c72c9c7406205a7fc7e73f4ae9d6db5b831e51b
mmolosay <mmolosays@gmail.com>
2024-01-23 00:55:07 +0100

    Add tests for 'ColorFactory'

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -15,4 +15,10 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${libs.versions.coroutines.get()}")
     implementation("javax.inject:javax.inject:1")
+
+    // Testing
+    testImplementation("junit:junit:${libs.versions.junit.get()}")
+    testImplementation("io.mockk:mockk:${libs.versions.mockk.get()}")
+    testImplementation("io.kotest:kotest-assertions-core:${libs.versions.kotestAssertions.get()}")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${libs.versions.coroutines.get()}")
 }
