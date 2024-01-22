1862ebeaee165399e19d3ffea4a7fe5b0dba54d0
mmolosay <mmolosays@gmail.com>
2024-01-22 16:21:54 +0100

    Update unit tests

--- presentation/color-input/src/test/java/io/github/mmolosay/thecolor/input/ColorInputRgbViewModelTest.kt
+++ presentation/color-input/src/test/java/io/github/mmolosay/thecolor/input/ColorInputRgbViewModelTest.kt
@@ -4,11 +4,14 @@ import io.github.mmolosay.thecolor.input.field.TextFieldUiData
 import io.github.mmolosay.thecolor.input.field.TextFieldUiData.Text
 import io.github.mmolosay.thecolor.input.field.TextFieldUiData.ViewData.TrailingIcon
 import io.github.mmolosay.thecolor.input.model.ColorInput
+import io.github.mmolosay.thecolor.input.model.UiState
 import io.github.mmolosay.thecolor.input.rgb.ColorInputRgbUiData
 import io.github.mmolosay.thecolor.input.rgb.ColorInputRgbViewData
 import io.github.mmolosay.thecolor.input.rgb.ColorInputRgbViewModel
 import io.github.mmolosay.thecolor.testing.MainDispatcherRule
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.beOfType
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
@@ -18,6 +21,7 @@ import io.mockk.runs
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
@@ -40,14 +44,48 @@ class ColorInputRgbViewModelTest {
     val viewData = ColorInputRgbViewData(rInputField, gInputField, bInputField)
 
     val mediator: ColorInputMediator = mockk {
-        every { rgbColorInputFlow } returns emptyFlow()
+        every { rgbColorInputFlow } returns flowOf(ColorInput.Rgb("", "", ""))
         coEvery { send(any()) } just runs
     }
 
     lateinit var sut: ColorInputRgbViewModel
 
-    val uiData: ColorInputRgbUiData
+    val uiState: UiState<ColorInputRgbUiData>
         get() = sut.uiStateFlow.value
+
+    val uiData: ColorInputRgbUiData
+        get() {
+            uiState should beOfType<UiState.Ready<*>>() // assertion for clear failure message
+            return (uiState as UiState.Ready).uiData
+        }
+
+    @Test
+    fun `sut is created with UiState BeingInitialized if mediator RGB flow has no value yet`() {
+        every { mediator.rgbColorInputFlow } returns emptyFlow()
+
+        createSut()
+
+        uiState should beOfType<UiState.BeingInitialized>()
+    }
+
+    @Test
+    fun `sut is created with UiState Ready if mediator RGB flow has value already`() {
+        createSut()
+
+        uiState should beOfType<UiState.Ready<*>>()
+    }
+
+    @Test
+    fun `UiState becomes Ready when mediator emits first value from RGB flow`() =
+        runTest(mainDispatcherRule.testDispatcher) {
+            val rgbColorInputFlow = MutableSharedFlow<ColorInput.Rgb>()
+            every { mediator.rgbColorInputFlow } returns rgbColorInputFlow
+            createSut()
+
+            rgbColorInputFlow.emit(ColorInput.Rgb("18", "1", "20"))
+
+            uiState should beOfType<UiState.Ready<*>>()
+        }
 
     @Test
     fun `filtering keeps only digits`() {
@@ -151,8 +189,10 @@ class ColorInputRgbViewModelTest {
             }
 
             uiData.rTextField.onTextChange(Text("18"))
+            uiData.gTextField.onTextChange(Text("1"))
+            uiData.bTextField.onTextChange(Text("20"))
 
-            val sentColorInput = ColorInput.Rgb(r = "18", g = "", b = "")
+            val sentColorInput = ColorInput.Rgb(r = "18", g = "1", b = "20")
             coVerify(exactly = 1) { mediator.send(sentColorInput) }
             collectionJob.cancel()
         }
