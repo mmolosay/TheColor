df05030ce7ee5401fe9fd5a834661f8ddcd91a85
mmolosay <mmolosays@gmail.com>
2024-02-23 20:01:28 +0100

    Add Compose preview

--- presentation/color-preview/src/main/java/io/github/mmolosay/thecolor/presentation/preview/ColorPreview.kt
+++ presentation/color-preview/src/main/java/io/github/mmolosay/thecolor/presentation/preview/ColorPreview.kt
@@ -2,14 +2,17 @@ package io.github.mmolosay.thecolor.presentation.preview
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.github.mmolosay.thecolor.presentation.design.TheColorTheme
 import io.github.mmolosay.thecolor.presentation.preview.ColorPreviewViewModel.State
 
 @Composable
@@ -17,15 +20,13 @@ fun ColorPreview(
     vm: ColorPreviewViewModel,
 ) {
     val state = vm.stateFlow.collectAsStateWithLifecycle().value
-    Box(
-        modifier = Modifier.size(48.dp),
-    ) {
-        when (state) {
-            is State.Absent -> Unit // show nothing
-            is State.Present -> {
-                val uiData = ColorPreviewUiData(state.data)
-                ColorPreview(uiData)
-            }
+    when (state) {
+        is State.Absent -> {
+            Spacer(modifier = Modifier.size(Size))
+        }
+        is State.Present -> {
+            val uiData = ColorPreviewUiData(state.data)
+            ColorPreview(uiData)
         }
     }
 }
@@ -36,8 +37,23 @@ fun ColorPreview(
 ) {
     Box(
         modifier = Modifier
-            .fillMaxSize()
+            .size(Size)
             .clip(CircleShape)
             .background(uiData.color),
     )
 }
+
+private val Size = 48.dp
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    TheColorTheme {
+        ColorPreview(uiData = previewUiData())
+    }
+}
+
+private fun previewUiData() =
+    ColorPreviewUiData(
+        color = Color(0xFF_13264D),
+    )
