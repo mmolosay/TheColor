c70c71f44bfb5a699a18893a8499a76fce1e1387
mmolosay <mmolosays@gmail.com>
2024-03-28 19:39:41 +0100

    Change application ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.ordolabs.thecolor" // TODO: rename
+        applicationId = "io.github.mmolosay.thecolor"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
     }
