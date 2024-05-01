cc54f976832e76b7eb3bd4b19c7a1cab792af07d
mmolosay <mmolosays@gmail.com>
2024-05-01 17:12:40 +0200

    Restore navigation bar when changing screen

--- app/src/main/java/io/github/mmolosay/thecolor/MainActivity.kt
+++ app/src/main/java/io/github/mmolosay/thecolor/MainActivity.kt
@@ -7,8 +7,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.mmolosay.thecolor.presentation.design.RestoreNavigationBarAsSideEffect
 import io.github.mmolosay.thecolor.presentation.design.TheColorTheme
 
 @AndroidEntryPoint
@@ -40,6 +43,14 @@ class MainActivity : AppCompatActivity() {
 @Composable
 private fun Application() {
     val navController = rememberNavController()
+
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    if (backStackEntry != null) {
+        // restore nav bar when screen changes.
+        // If the particular screen wants to style nav bar in its own way, it may do so
+        RestoreNavigationBarAsSideEffect()
+    }
+
     MainNavHost(
         navController = navController,
     )
