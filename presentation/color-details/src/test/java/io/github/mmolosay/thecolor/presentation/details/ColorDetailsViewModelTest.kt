23332ba491c42c797a5ff2e4a89b1e3c28889abe
mmolosay <mmolosays@gmail.com>
2024-08-04 18:38:45 +0200

    Add unit tests for "try again" feature

--- presentation/color-details/src/test/java/io/github/mmolosay/thecolor/presentation/details/ColorDetailsViewModelTest.kt
+++ presentation/color-details/src/test/java/io/github/mmolosay/thecolor/presentation/details/ColorDetailsViewModelTest.kt
@@ -300,6 +300,95 @@ class ColorDetailsViewModelTest {
             }
         }
 
+    /**
+     * GIVEN
+     *  1. fetching color details will end with failure.
+     *  2. SUT is initialized.
+     *
+     * WHEN
+     *  [FetchData][ColorCenterCommand.FetchData] command is emitted and data fetching ends with failure
+     *
+     * THEN
+     *  updated data state is [DataState.Error].
+     */
+    @Test
+    fun `emission of 'fetch data' command that triggers failing data fetching results in emission of 'DataState Error'`() =
+        runTest(mainDispatcherRule.testDispatcher) {
+            val commandFlow = MutableSharedFlow<ColorCenterCommand>()
+            every { commandProvider.commandFlow } returns commandFlow
+            coEvery { getColorDetails(color = any()) } returns
+                HttpFailure.UnknownHost(cause = mockk())
+            createSut()
+
+            val command = ColorCenterCommand.FetchData(color = mockk(), colorRole = null)
+            commandFlow.emit(command)
+
+            sut.dataStateFlow.value should beOfType<DataState.Error>()
+        }
+
+    /**
+     * GIVEN
+     *  1. SUT is initialized.
+     *  2. [FetchData][ColorCenterCommand.FetchData] command with color X is emitted and initial data is fetched.
+     *  3. next [getColorDetails] will end with failure.
+     *  4. new [FetchData][ColorCenterCommand.FetchData] command with color Y is emitted, but this time
+     *  data fetching returns failure and data state is set to [DataState.Error].
+     *  5. next [getColorDetails] will end with success.
+     *
+     * WHEN
+     *  [ColorDetailsError.tryAgain] is invoked
+     *
+     * THEN
+     *  data is fetched successfully for color Y from last [FetchData][ColorCenterCommand.FetchData]
+     *  command.
+     */
+    @Test
+    fun `invoking 'try again' action of 'DataState Error' will use color from last 'fetch data' command`() =
+        runTest(mainDispatcherRule.testDispatcher) {
+            fun mockGetColorDetailsReturnsSuccess() {
+                val fetchedDetails: ColorDetails = mockk(relaxed = true) {
+                    every { this@mockk.color } returns Color.Hex(0x000000) // doesn't matter
+                    every { exact.color } returns Color.Hex(0x000000) // doesn't matter
+                }
+                coEvery { getColorDetails(color = any()) } returns
+                    Result.Success(value = fetchedDetails)
+            }
+
+            val commandFlow = MutableSharedFlow<ColorCenterCommand>()
+            every { commandProvider.commandFlow } returns commandFlow
+            mockGetColorDetailsReturnsSuccess()
+            every {
+                createDataMock(
+                    details = any(),
+                    goToExactColor = any(),
+                    initialColor = any(),
+                    goToInitialColor = any(),
+                )
+            } returns mockk()
+            createSut()
+            val color1 = Color.Hex(0x111111)
+            val command1 = ColorCenterCommand.FetchData(
+                color = color1, colorRole = null
+            )
+            commandFlow.emit(command1)
+            coEvery { getColorDetails(color = any()) } returns
+                HttpFailure.UnknownHost(cause = mockk())
+            val color2 = Color.Hex(0x222222)
+            val command2 = ColorCenterCommand.FetchData(
+                color = color2, colorRole = null
+            )
+            commandFlow.emit(command2)
+            mockGetColorDetailsReturnsSuccess()
+
+            sut.dataStateFlow.value.shouldBeInstanceOf<DataState.Error>().error.tryAgain()
+
+            val colors = mutableListOf<Color>()
+            coVerify {
+                getColorDetails.invoke(color = capture(colors))
+            }
+            colors.last() shouldBe color2
+        }
+
     fun createSut(
         createData: CreateColorDetailsDataUseCase = createDataMock,
     ) =
