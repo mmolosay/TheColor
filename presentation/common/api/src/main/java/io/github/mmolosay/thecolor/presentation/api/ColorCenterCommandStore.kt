8181aad13682b8def75f5944fd77de41e0d27719
mmolosay <mmolosays@gmail.com>
2024-08-20 22:05:30 +0200

    Add comment

--- presentation/common/api/src/main/java/io/github/mmolosay/thecolor/presentation/api/ColorCenterCommandStore.kt
+++ presentation/common/api/src/main/java/io/github/mmolosay/thecolor/presentation/api/ColorCenterCommandStore.kt
@@ -9,6 +9,22 @@ import javax.inject.Inject
 /**
  * Storage that holds a [Flow] of [ColorCenterCommand]s for a color center ViewModel to handle.
  */
+/*
+ * One may think that this file belongs to Color Center feature, and thus should be located at
+ * ':presentation:color-center:api' module.
+ * But it is not true. Consider following facts:
+ * 1. Components in this file are used by "child" features of Color Center feature, which are
+ * Color Details and Color Scheme.
+ * 2. Color Center module depends on its "child" modules.
+ *
+ * Thus, by putting these classes in Color Center, it will create a circular dependency, which
+ * must be avoided.
+ *
+ * Ideally, these classes should be put in a module that is "common" for "child" features of
+ * Color Center. However, it's unnecessary to spawn such atomic modules for just a file or two.
+ * Instead, I've put them in ':presentation:common:api" module, which is just one level higher
+ * than this ideal module it was theorised about prior.
+ */
 class ColorCenterCommandStore @Inject constructor() : ColorCenterCommandProvider {
 
     private val _commandFlow = MutableSharedFlow<ColorCenterCommand>(replay = 1)
--- presentation/common/api/src/main/java/io/github/mmolosay/thecolor/presentation/api/ColorCenterEventStore.kt
+++ presentation/common/api/src/main/java/io/github/mmolosay/thecolor/presentation/api/ColorCenterEventStore.kt
@@ -10,6 +10,22 @@ import javax.inject.Inject
  * Storage that holds a [Flow] of [ColorCenterEvent]s from a color center View and its sub-Views:
  * color details View and color scheme View.
  */
+/*
+ * One may think that this file belongs to Color Center feature, and thus should be located at
+ * ':presentation:color-center:api' module.
+ * But it is not true. Consider following facts:
+ * 1. Components in this file are used by "child" features of Color Center feature, which are
+ * Color Details and Color Scheme.
+ * 2. Color Center module depends on its "child" modules.
+ *
+ * Thus, by putting these classes in Color Center, it will create a circular dependency, which
+ * must be avoided.
+ *
+ * Ideally, these classes should be put in a module that is "common" for "child" features of
+ * Color Center. However, it's unnecessary to spawn such atomic modules for just a file or two.
+ * Instead, I've put them in ':presentation:common:api" module, which is just one level higher
+ * than this ideal module it was theorised about prior.
+ */
 class ColorCenterEventStore @Inject constructor() : ColorCenterEventProvider {
 
     private val _eventFlow = MutableSharedFlow<ColorCenterEvent>(replay = 0)
