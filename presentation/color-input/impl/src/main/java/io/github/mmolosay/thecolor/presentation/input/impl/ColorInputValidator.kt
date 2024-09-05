b5e3875077566b633498c38a858158e407823759
mmolosay <mmolosays@gmail.com>
2024-09-05 22:33:40 +0200

    Add unit tests for 'ColorInputValidator'

--- presentation/color-input/impl/src/main/java/io/github/mmolosay/thecolor/presentation/input/impl/ColorInputValidator.kt
+++ presentation/color-input/impl/src/main/java/io/github/mmolosay/thecolor/presentation/input/impl/ColorInputValidator.kt
@@ -11,7 +11,6 @@ import javax.inject.Singleton
 /**
  * Validates given [ColorInput] and returns the result as [ColorInputState].
  */
-// TODO: add unit tests
 @Singleton
 class ColorInputValidator @Inject constructor(
     private val colorInputMapper: ColorInputMapper,
