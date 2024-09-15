52d7f7df0d3ace187bfd4812a996d69b9f88ff04
mmolosay <mmolosays@gmail.com>
2024-09-15 15:22:07 +0200

    Add overloads that take (almost) primitive parameters

--- presentation/color-details/src/main/java/io/github/mmolosay/thecolor/presentation/details/ColorDetailsOnTintedSurface.kt
+++ presentation/color-details/src/main/java/io/github/mmolosay/thecolor/presentation/details/ColorDetailsOnTintedSurface.kt
@@ -1,6 +1,7 @@
 package io.github.mmolosay.thecolor.presentation.details
 
 import androidx.compose.ui.graphics.Color
+import io.github.mmolosay.thecolor.presentation.api.ColorInt
 import io.github.mmolosay.thecolor.presentation.design.ColorsOnTintedSurface
 import io.github.mmolosay.thecolor.presentation.design.colorsOnDarkSurface
 import io.github.mmolosay.thecolor.presentation.design.colorsOnLightSurface
@@ -12,10 +13,14 @@ import io.github.mmolosay.thecolor.presentation.impl.toCompose
 object ColorDetailsOnTintedSurfaceDefaults {
 
     fun surfaceColor(seedData: ColorDetailsSeedData): Color =
-        seedData.color.toCompose()
+        surfaceColor(seedColor = seedData.color)
 
-    fun colorsOnTintedSurface(seedData: ColorDetailsSeedData): ColorsOnTintedSurface {
-        val useLight = seedData.isDark
-        return if (useLight) colorsOnDarkSurface() else colorsOnLightSurface()
-    }
+    fun surfaceColor(seedColor: ColorInt): Color =
+        seedColor.toCompose()
+
+    fun colorsOnTintedSurface(seedData: ColorDetailsSeedData): ColorsOnTintedSurface =
+        colorsOnTintedSurface(useLight = seedData.isDark)
+
+    fun colorsOnTintedSurface(useLight: Boolean): ColorsOnTintedSurface =
+        if (useLight) colorsOnDarkSurface() else colorsOnLightSurface()
 }
