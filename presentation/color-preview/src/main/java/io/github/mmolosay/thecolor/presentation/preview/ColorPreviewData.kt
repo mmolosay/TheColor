58622ddbbb5da7a812f81d56eca61bf67a627025
mmolosay <mmolosays@gmail.com>
2024-09-22 01:18:28 +0200

    Cleanup imports

--- presentation/color-preview/src/main/java/io/github/mmolosay/thecolor/presentation/preview/ColorPreviewData.kt
+++ presentation/color-preview/src/main/java/io/github/mmolosay/thecolor/presentation/preview/ColorPreviewData.kt
@@ -6,5 +6,5 @@ import io.github.mmolosay.thecolor.presentation.api.ColorInt
  * Platform-agnostic data provided by ViewModel to color preview View.
  */
 data class ColorPreviewData(
-    val color: io.github.mmolosay.thecolor.presentation.api.ColorInt?,
+    val color: ColorInt?,
 )
