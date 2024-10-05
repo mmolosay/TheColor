110e0b9909e612bfb06e847ee48e2c8d5ac1798e
mmolosay <mmolosays@gmail.com>
2024-10-05 16:50:19 +0200

    Delete old/unused components

--- presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/ColorCenterSessionManager.kt
+++
@@ -1,9 +0,0 @@
-package io.github.mmolosay.thecolor.presentation.home
-
-import io.github.mmolosay.thecolor.presentation.input.api.ColorInputEvent
-import javax.inject.Inject
-
-internal class ColorCenterSessionManager @Inject constructor() {
-
-
-}
