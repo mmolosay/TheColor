30a8238c537da2e472fc663e230d87c91752d9fb
mmolosay <mmolosays@gmail.com>
2024-10-17 22:19:52 +0200

    Refine KDoc

--- presentation/design-system/src/main/java/io/github/mmolosay/thecolor/presentation/design/UiThemeResolver.kt
+++ presentation/design-system/src/main/java/io/github/mmolosay/thecolor/presentation/design/UiThemeResolver.kt
@@ -2,6 +2,7 @@ package io.github.mmolosay.thecolor.presentation.design
 
 /**
  * Defines a strategy of obtaining a [UiTheme] for a provided [Brightness][UiTheme.Brightness].
+ * Brightness is usually inferred from system UI mode (see [systemBrightness]).
  */
 fun interface UiThemeResolver {
     fun resolve(brightness: UiTheme.Brightness): UiTheme
