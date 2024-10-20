ef85c460711aa13b250c9810ad04d68ef121c187
mmolosay <mmolosays@gmail.com>
2024-10-20 21:50:07 +0200

    Revert "Bump Compose navigation version"
    This reverts commit 0051997e17ec37b07481b2762a1969524bce355d.
    
    The new version was causing java.lang.NoSuchMethodError: No static method HorizontalPager...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     val lifecycleVersion = "2.7.0" // TODO: move to version catalogue
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
