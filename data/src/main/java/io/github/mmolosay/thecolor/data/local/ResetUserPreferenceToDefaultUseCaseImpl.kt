f065350533699c064a93cf8f747c778a7f59e1cd
mmolosay <mmolosays@gmail.com>
2024-11-10 23:59:33 +0200

    Add comment

--- data/src/main/java/io/github/mmolosay/thecolor/data/local/ResetUserPreferenceToDefaultUseCaseImpl.kt
+++ data/src/main/java/io/github/mmolosay/thecolor/data/local/ResetUserPreferenceToDefaultUseCaseImpl.kt
@@ -2,14 +2,18 @@ package io.github.mmolosay.thecolor.data.local
 
 import io.github.mmolosay.thecolor.domain.repository.UserPreferencesRepository
 import io.github.mmolosay.thecolor.domain.usecase.ResetUserPreferencesToDefaultUseCase
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.time.TimeSource
+import kotlin.time.measureTime
 
 class ResetUserPreferenceToDefaultUseCaseImpl @Inject constructor(
     private val userPreferencesRepository: UserPreferencesRepository,
 ) : ResetUserPreferencesToDefaultUseCase {
 
     override suspend fun invoke() {
-        // TODO: try concurrent updates
+        // updating values sequentially and concurrently take about the same time ~68ms
         userPreferencesRepository.setColorInputType(null)
         userPreferencesRepository.setAppUiColorSchemeSet(null)
         userPreferencesRepository.setResumeFromLastSearchedColorOnStartup(null)
