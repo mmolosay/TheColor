c7d744d162c54b1bc5f64d96b9c42846c8f8d275
mmolosay <mmolosays@gmail.com>
2024-11-30 17:26:39 +0200

    Add a @Preview

--- presentation/color-scheme/src/main/java/io/github/mmolosay/thecolor/presentation/scheme/SelectedSwatchDetailsDialog.kt
+++ presentation/color-scheme/src/main/java/io/github/mmolosay/thecolor/presentation/scheme/SelectedSwatchDetailsDialog.kt
@@ -2,6 +2,7 @@ package io.github.mmolosay.thecolor.presentation.scheme
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,17 +14,24 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.github.mmolosay.thecolor.presentation.api.ColorInt
 import io.github.mmolosay.thecolor.presentation.api.NavBarAppearanceStack
+import io.github.mmolosay.thecolor.presentation.design.ColorsOnTintedSurface
+import io.github.mmolosay.thecolor.presentation.design.TheColorTheme
+import io.github.mmolosay.thecolor.presentation.design.colorsOnDarkSurface
 import io.github.mmolosay.thecolor.presentation.details.ColorDetails
 import io.github.mmolosay.thecolor.presentation.details.ColorDetailsCrossfade
+import io.github.mmolosay.thecolor.presentation.details.ColorDetailsData
 import io.github.mmolosay.thecolor.presentation.details.ColorDetailsOnTintedSurfaceDefaults
+import io.github.mmolosay.thecolor.presentation.details.ColorDetailsSeedData
 import io.github.mmolosay.thecolor.presentation.details.ColorDetailsViewModel
 import io.github.mmolosay.thecolor.presentation.impl.TintedSurface
 
 // This piece of UI doesn't have its own "UI" model.
-// TODO: add @Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SelectedSwatchDetailsDialog(
@@ -31,9 +39,24 @@ internal fun SelectedSwatchDetailsDialog(
     colorSchemeUiData: ColorSchemeUiData,
     navBarAppearanceStack: NavBarAppearanceStack,
 ) {
-    val seedData = viewModel.currentSeedDataFlow.collectAsStateWithLifecycle().value ?: return
+    SelectedSwatchDetailsDialog(
+        onDismissRequest = colorSchemeUiData.onSelectedSwatchDetailsDialogDismissRequest,
+        seedData = viewModel.currentSeedDataFlow.collectAsStateWithLifecycle().value ?: return,
+        colorDetailsDataState = viewModel.dataStateFlow.collectAsStateWithLifecycle().value,
+        navBarAppearanceStack = navBarAppearanceStack,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun SelectedSwatchDetailsDialog(
+    onDismissRequest: () -> Unit,
+    seedData: ColorDetailsSeedData,
+    colorDetailsDataState: ColorDetailsViewModel.DataState,
+    navBarAppearanceStack: NavBarAppearanceStack,
+) {
     val surfaceColor = ColorDetailsOnTintedSurfaceDefaults.surfaceColor(seedData)
-    val contentColors = ColorDetailsOnTintedSurfaceDefaults.colorsOnTintedSurface(seedData)
+    val colorsOnTintedSurface = ColorDetailsOnTintedSurfaceDefaults.colorsOnTintedSurface(seedData)
     val windowInsets = BottomSheetDefaults.windowInsets
     val bottomSheetWindowInsets = run {
         val sides = WindowInsetsSides.Horizontal + WindowInsetsSides.Top
@@ -41,48 +64,96 @@ internal fun SelectedSwatchDetailsDialog(
     }
 
     ModalBottomSheet(
-        onDismissRequest = colorSchemeUiData.onSelectedSwatchDetailsDialogDismissRequest,
+        onDismissRequest = onDismissRequest,
         containerColor = surfaceColor,
-        contentColor = contentColors.accent,
+        contentColor = colorsOnTintedSurface.accent,
         dragHandle = {
             BottomSheetDefaults.DragHandle(
-                color = contentColors.muted,
+                color = colorsOnTintedSurface.muted,
             )
         },
         windowInsets = bottomSheetWindowInsets,
     ) {
-        TintedSurface(
-            surfaceColor = surfaceColor,
-            contentColors = contentColors,
-        ) {
-            Column {
-                ColorDetailsCrossfade(
-                    actualDataState = viewModel.dataStateFlow.collectAsStateWithLifecycle().value,
-                ) { state ->
-                    ColorDetails(
-                        state = state,
-                        modifier = Modifier.padding(bottom = 24.dp),
-                    )
-                }
+        Content(
+            surfaceColor = Color.Unspecified, // already has a background due to ModalBottomSheet's 'containerColor'
+            colorsOnTintedSurface = colorsOnTintedSurface,
+            colorDetailsDataState = colorDetailsDataState,
+            windowInsets = windowInsets,
+        )
+    }
 
-                val bottomWindowInsets = windowInsets.only(WindowInsetsSides.Bottom)
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .windowInsetsBottomHeight(bottomWindowInsets)
-                        .consumeWindowInsets(bottomWindowInsets)
+//    DisposableEffect(seedData) {
+//        val appearance = NavBarAppearance(
+//            color = seedData.color.toArgb(),
+//            useLightTintForControls = seedData.isDark,
+//        )
+//        navBarAppearanceStack.push(appearance)
+//        onDispose {
+//            navBarAppearanceStack.peel()
+//        }
+//    }
+}
+
+@Composable
+private fun Content(
+    surfaceColor: Color,
+    colorsOnTintedSurface: ColorsOnTintedSurface,
+    colorDetailsDataState: ColorDetailsViewModel.DataState,
+    windowInsets: WindowInsets,
+) {
+    TintedSurface(
+        surfaceColor = surfaceColor,
+        contentColors = colorsOnTintedSurface,
+    ) {
+        Column {
+            ColorDetailsCrossfade(
+                actualDataState = colorDetailsDataState,
+            ) { state ->
+                ColorDetails(
+                    state = state,
+                    modifier = Modifier.padding(bottom = 24.dp),
                 )
             }
-//            DisposableEffect(seedData) {
-//                val appearance = NavBarAppearance(
-//                    color = seedData.color.toArgb(),
-//                    useLightTintForControls = seedData.isDark,
-//                )
-//                navBarAppearanceStack.push(appearance)
-//                onDispose {
-//                    navBarAppearanceStack.peel()
-//                }
-//            }
+
+            val bottomWindowInsets = windowInsets.only(WindowInsetsSides.Bottom)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .windowInsetsBottomHeight(bottomWindowInsets)
+                    .consumeWindowInsets(bottomWindowInsets)
+            )
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+// for some reason, ModalBottomSheet doesn't show in @Preview:
+// https://issuetracker.google.com/issues/283843380
+private fun Preview() {
+    TheColorTheme {
+        Content(
+            surfaceColor = Color(0xFF_1A803F),
+            colorsOnTintedSurface = colorsOnDarkSurface(),
+            colorDetailsDataState = ColorDetailsViewModel.DataState.Ready(
+                ColorDetailsData(
+                    colorName = "Jewel",
+                    hex = ColorDetailsData.Hex("#1A803F"),
+                    rgb = ColorDetailsData.Rgb("26", "128", "63"),
+                    hsl = ColorDetailsData.Hsl("142", "66", "30"),
+                    hsv = ColorDetailsData.Hsv("142", "80", "50"),
+                    cmyk = ColorDetailsData.Cmyk("80", "0", "51", "50"),
+                    exactMatch = ColorDetailsData.ExactMatch.No(
+                        exactValue = "#126B40",
+                        exactColor = ColorInt(0x126B40),
+                        goToExactColor = { },
+                        deviation = "1366",
+                    ),
+                    initialColorData = null,
+                )
+            ),
+            windowInsets = BottomSheetDefaults.windowInsets
+        )
     }
 }
