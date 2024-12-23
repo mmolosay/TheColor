157824078c0305fbb455911826df3839f8237786
mmolosay <mmolosays@gmail.com>
2024-12-23 17:44:52 +0100

    Use automatic tag (class name)

--- data/src/main/java/io/github/mmolosay/thecolor/data/local/TouchLocalDatabaseUseCaseImpl.kt
+++ data/src/main/java/io/github/mmolosay/thecolor/data/local/TouchLocalDatabaseUseCaseImpl.kt
@@ -23,18 +23,14 @@ class TouchLocalDatabaseUseCaseImpl @Inject constructor(
             val elapsed = measureTime {
                 userPreferences.data.first()
             }
-            Timber
-                .tag(this::class.simpleName.orEmpty())
-                .i("Touching \'User Preferences\' DB took $elapsed.")
+            Timber.i("Touching \'User Preferences\' DB took $elapsed.")
         }
         // second and subsequent accesses to DataStore don't seem to be as effective as the first one
         kotlin.run {
             val elapsed = measureTime {
                 miscValues.data.first()
             }
-            Timber
-                .tag(this::class.simpleName.orEmpty())
-                .i("Touching \'Misc Values\' DB took $elapsed.")
+            Timber.i("Touching \'Misc Values\' DB took $elapsed.")
         }
     }
 }
