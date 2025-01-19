0e0a446175ea297deb73342b79bf1999f1c95f8c
mmolosay <mmolosays@gmail.com>
2025-01-19 23:58:25 +0100

    Manually coerce result RGB into 0-255 component range

--- data/src/main/java/io/github/mmolosay/thecolor/data/DimColorUseCaseImpl.kt
+++ data/src/main/java/io/github/mmolosay/thecolor/data/DimColorUseCaseImpl.kt
@@ -4,6 +4,7 @@ import com.github.ajalt.colormath.model.RGB
 import com.github.ajalt.colormath.model.RGB.Companion.invoke
 import io.github.mmolosay.thecolor.data.remote.mapper.ColorMapper
 import io.github.mmolosay.thecolor.domain.model.Color
+import io.github.mmolosay.thecolor.domain.model.ColorConstants
 import io.github.mmolosay.thecolor.domain.usecase.ColorConverter
 import io.github.mmolosay.thecolor.domain.usecase.DimColorUseCase
 import javax.inject.Inject
@@ -24,6 +25,16 @@ class DimColorUseCaseImpl @Inject constructor(
         val dimmedRgb = lab
             .copy(l = desiredLightness * 100) // convert back into range 0..100
             .toSRGB()
-        return Color.Rgb(dimmedRgb.redInt, dimmedRgb.greenInt, dimmedRgb.blueInt)
+        /*
+         * There's a bug in Colormath that produces negative 0-255 green component when
+         * input color of this function is #F9031B
+         */
+        fun Int.coerceInRgbComponentRange() =
+            this.coerceIn(ColorConstants.RgbColorComponentIntRange)
+        return Color.Rgb(
+            r = dimmedRgb.redInt.coerceInRgbComponentRange(),
+            g = dimmedRgb.greenInt.coerceInRgbComponentRange(),
+            b = dimmedRgb.blueInt.coerceInRgbComponentRange(),
+        )
     }
 }
