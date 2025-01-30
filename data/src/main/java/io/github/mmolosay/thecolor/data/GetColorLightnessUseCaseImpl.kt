3aab6ecc3238620b970335c21c2fbc6ab748526c
mmolosay <mmolosays@gmail.com>
2025-01-30 23:18:21 +0100

    Use sRGB color scheme implicitly

--- data/src/main/java/io/github/mmolosay/thecolor/data/GetColorLightnessUseCaseImpl.kt
+++ data/src/main/java/io/github/mmolosay/thecolor/data/GetColorLightnessUseCaseImpl.kt
@@ -1,6 +1,6 @@
 package io.github.mmolosay.thecolor.data
 
-import com.github.ajalt.colormath.model.RGB
+import com.github.ajalt.colormath.model.SRGB
 import io.github.mmolosay.thecolor.data.remote.mapper.ColorMapper
 import io.github.mmolosay.thecolor.domain.model.Color
 import io.github.mmolosay.thecolor.domain.usecase.ColorConverter
@@ -20,14 +20,14 @@ class GetColorLightnessUseCaseImpl @Inject constructor(
     override fun Color.hslLightness(): Float {
         val hex = with(colorConverter) { toHex() }
         val hexString = with(colorMapper) { hex.toHexString() }
-        val hsl = RGB(hex = hexString).toHSL()
+        val hsl = SRGB(hex = hexString).toHSL()
         return hsl.l
     }
 
     override fun Color.labLightness(): Float {
         val hex = with(colorConverter) { toHex() }
         val hexString = with(colorMapper) { hex.toHexString() }
-        val lab = RGB(hex = hexString).toLAB()
+        val lab = SRGB(hex = hexString).toLAB()
         return (lab.l / 100) // LAB's L is in range 0..100, but we want in range 0..1
     }
 }
