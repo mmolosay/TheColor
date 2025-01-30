ba9bad5acbc6980f60f203d72c3a3da959d22f34
mmolosay <mmolosays@gmail.com>
2025-01-30 23:13:40 +0100

    Add LAB gamuts

--- domain/src/main/java/io/github/mmolosay/thecolor/domain/usecase/ColorMath.kt
+++ domain/src/main/java/io/github/mmolosay/thecolor/domain/usecase/ColorMath.kt
@@ -1,6 +1,7 @@
 package io.github.mmolosay.thecolor.domain.usecase
 
 import io.github.mmolosay.thecolor.domain.model.Color
+import io.github.mmolosay.thecolor.utils.maxAbs
 
 /**
  * Performs mathematical operations with colors.
@@ -17,5 +18,11 @@ interface ColorMath {
         val l: Float,
         val a: Float,
         val b: Float,
-    )
+    ) {
+        companion object {
+            val LGamut = 0f..100f
+            val AGamut = -128f..+127f
+            val BGamut = -128f..+127f
+        }
+    }
 }
