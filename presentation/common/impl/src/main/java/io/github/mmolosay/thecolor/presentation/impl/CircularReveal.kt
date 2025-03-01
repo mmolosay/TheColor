27de074eb8879d57b5f1676108817493b780eed7
mmolosay <mmolosays@gmail.com>
2025-03-02 00:18:06 +0100

    Introduce `CircularReveal()` Composable

--- presentation/common/impl/src/main/java/io/github/mmolosay/thecolor/presentation/impl/CircularReveal.kt
+++ presentation/common/impl/src/main/java/io/github/mmolosay/thecolor/presentation/impl/CircularReveal.kt
@@ -15,8 +15,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
@@ -31,8 +36,34 @@ import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.mmolosay.thecolor.presentation.design.TheColorTheme
+import io.github.mmolosay.thecolor.presentation.impl.CircularRevealAnimator.Companion.FullyCollapsedValue
 import kotlinx.coroutines.launch
 import kotlin.math.hypot
+
+/**
+ * A container for the [content] to which circular reveal animation is applied to.
+ * Only composes [content] if it would be visible considering current animation state.
+ */
+@Composable
+fun CircularReveal(
+    animator: CircularRevealAnimator,
+    content: @Composable () -> Unit,
+) {
+    fun shouldComposeContent(): Boolean {
+        val isFullyCollapsed = (animator.progressAnimatable.value == FullyCollapsedValue)
+        return !isFullyCollapsed
+    }
+    // animation is started in LaunchedEffect(), thus only on next frame.
+    // calculating result of 'shouldComposeContent()' in the next frame as well.
+    var composeContent by remember { mutableStateOf(shouldComposeContent()) }
+    LaunchedEffect(animator.progressAnimatable.value) {
+        composeContent = shouldComposeContent()
+    }
+
+    if (composeContent) {
+        content()
+    }
+}
 
 /**
  * Clips the content to a circle shape.
