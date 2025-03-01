cbe5e8c07720c913725f4712ca7c38996e673259
mmolosay <mmolosays@gmail.com>
2025-03-02 00:26:02 +0100

    Use `derivedStateOf`

--- presentation/common/impl/src/main/java/io/github/mmolosay/thecolor/presentation/impl/CircularReveal.kt
+++ presentation/common/impl/src/main/java/io/github/mmolosay/thecolor/presentation/impl/CircularReveal.kt
@@ -15,13 +15,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
@@ -49,17 +46,12 @@ fun CircularReveal(
     animator: CircularRevealAnimator,
     content: @Composable () -> Unit,
 ) {
-    fun shouldComposeContent(): Boolean {
-        val isFullyCollapsed = (animator.progressAnimatable.value == FullyCollapsedValue)
-        return !isFullyCollapsed
+    val composeContent by remember {
+        derivedStateOf {
+            val isFullyCollapsed = (animator.progressAnimatable.value == FullyCollapsedValue)
+            !isFullyCollapsed
+        }
     }
-    // animation is started in LaunchedEffect(), thus only on next frame.
-    // calculating result of 'shouldComposeContent()' in the next frame as well.
-    var composeContent by remember { mutableStateOf(shouldComposeContent()) }
-    LaunchedEffect(animator.progressAnimatable.value) {
-        composeContent = shouldComposeContent()
-    }
-
     if (composeContent) {
         content()
     }
