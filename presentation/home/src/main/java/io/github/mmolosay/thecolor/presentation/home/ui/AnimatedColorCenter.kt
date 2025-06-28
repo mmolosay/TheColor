63cc31424077bd43a12c64e5b089c58c0db0e004
mmolosay <mmolosays@gmail.com>
2025-06-28 17:42:40 +0200

    Remove debug alpha

--- presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/ui/AnimatedColorCenter.kt
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/ui/AnimatedColorCenter.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.center
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -69,7 +68,6 @@ internal fun AnimatedColorCenter(
         var visibleHeightInParent by remember { mutableStateOf<Float?>(null) }
         Box(
             modifier = Modifier
-                .alpha(0.5f) // TODO: remove me
                 .onGloballyPositioned { coordinates ->
                     val ownPosInParent = coordinates.positionInParent()
                     visibleHeightInParent = calcVisibleHeightInScrollableContainer(
