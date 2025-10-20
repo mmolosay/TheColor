e66290760012dbbb2454ec75f659d52e149ab6b1
mmolosay <mmolosays@gmail.com>
2025-10-21 00:45:01 +0200

    Remove TODO

--- presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/viewmodel/HomeViewModel.kt
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/viewmodel/HomeViewModel.kt
@@ -471,7 +471,7 @@ class HomeViewModel @Inject constructor(
         val sessionState = ccSessionStore.sessionState
         return when (sessionState) {
             is SessionState.NoSession -> false // no session -> nothing to belong to
-            is SessionState.BeingBuilt -> (sessionState.seed == color) // TODO: this condition is a transitive assumption from 'DoesColorBelongToSessionUseCase'. Remove? Throw error if the SessionState is BeingBuilt?
+            is SessionState.BeingBuilt -> (sessionState.seed == color) // started this session
             is SessionState.Ongoing -> with(doesColorBelongToSession) { color doesBelongTo sessionState.session }
         }
     }
