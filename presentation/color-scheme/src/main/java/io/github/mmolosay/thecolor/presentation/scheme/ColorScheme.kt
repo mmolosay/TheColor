fcd5660f871fd42cc17cdaded37e8d6318c9b6f1
mmolosay <mmolosays@gmail.com>
2025-11-07 19:16:38 +0100

    Refactor swatch list to pager

--- presentation/color-scheme/src/main/java/io/github/mmolosay/thecolor/presentation/scheme/ColorScheme.kt
+++ presentation/color-scheme/src/main/java/io/github/mmolosay/thecolor/presentation/scheme/ColorScheme.kt
@@ -10,22 +10,29 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerSnapDistance
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
@@ -40,6 +47,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -47,8 +56,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalContext
@@ -56,8 +70,11 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import io.github.mmolosay.thecolor.presentation.common.annotation
 import io.github.mmolosay.thecolor.presentation.common.colorint.ColorInt
 import io.github.mmolosay.thecolor.presentation.common.colorint.toCompose
@@ -74,6 +91,7 @@ import io.github.mmolosay.thecolor.presentation.errors.message
 import io.github.mmolosay.thecolor.presentation.errors.rememberDefaultErrorsUiStrings
 import io.github.mmolosay.thecolor.presentation.scheme.ColorSchemeViewModel.DataState
 import io.github.mmolosay.thecolor.utils.doNothing
+import kotlin.math.absoluteValue
 import io.github.mmolosay.thecolor.domain.model.ColorScheme as DomainColorScheme
 import io.github.mmolosay.thecolor.presentation.design.R as DesignR
 
@@ -136,50 +154,160 @@ fun ColorScheme(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun Swatches(
     swatches: List<ColorSchemeData.Swatch>,
     onSwatchClick: (indexOfSwatch: Int) -> Unit,
 ) {
-    val scrollState = rememberScrollState()
-    Box(
-        modifier = Modifier
-            .edgeToEdge(parentTotalHorizontalPadding = 32.dp)
-            .fillMaxWidth()
-            .horizontalScroll(
-                state = scrollState,
-            ),
-        contentAlignment = Alignment.Center,
-    ) {
-        Row(
-            modifier = Modifier.padding(horizontal = 48.dp), // equivalent of content padding in LazyList
-            horizontalArrangement = Arrangement.spacedBy((-32).dp),
-        ) {
-            swatches.forEachIndexed { index, swatch ->
-                Swatch(
-                    swatch = swatch,
-                    onClick = { onSwatchClick(index) },
-                )
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        pageCount = { swatches.size },
+    )
+    val calculatedContentPadding = run {
+        // 'rememberPagerState()' employs 'rememberSaveable()', which allows to recall last value on "first" composition
+        val lastSettledPage = remember { pagerState.settledPage }
+        val padding by calcContentPadding(pagerState)
+        if (padding != null) { // assuming it can go from null to not-null only once
+            LaunchedEffect(Unit) {
+                pagerState.scrollToPage(page = lastSettledPage)
             }
+        }
+        return@run padding
+    }
+    val isReadyToBeDrawn = (calculatedContentPadding != null)
+    HorizontalPager(
+        modifier = Modifier
+            .drawIf(isReadyToBeDrawn)
+            .fillMaxWidth()
+            .edgeToEdge(parentTotalHorizontalPadding = 32.dp),
+//            .drawCenterItemPointer() // TODO: remove me
+        state = pagerState,
+        contentPadding = calculatedContentPadding ?: PaddingValues(0.dp),
+        pageSize = PageSize.Fixed(pageSize = SwatchSize),
+        pageSpacing = SwatchSpacing,
+        flingBehavior = PagerDefaults.flingBehavior(
+            state = pagerState,
+            pagerSnapDistance = PagerSnapDistance.atMost(pages = swatches.size),
+        ),
+        snapPosition = SnapPosition.Center,
+    ) { pageIndex ->
+        val swatch = swatches[pageIndex]
+        val zIndex by remember {
+            derivedStateOf {
+                val currentPage = pagerState.currentPage
+                val maxPageIndex = pagerState.pageCount - 1
+                fun indexFraction() = (pageIndex.toFloat() / maxPageIndex)
+                when {
+                    pageIndex == currentPage -> 2f // biggest z-index to draw current page above any other
+                    // if calculated z-index doesn't depend on current page, then only two swatches will recompose: old current page and new one
+                    pageIndex < currentPage ->
+                        indexFraction() // increasing z-index before current page
+                    pageIndex > currentPage ->
+                        1f - indexFraction() // decreasing z-index after current page
+                    else -> error("unreachable")
+                }
+            }
+        }
+        val scale by remember {
+            derivedStateOf {
+                val currentPageWithOffset =
+                    pagerState.currentPage + pagerState.currentPageOffsetFraction
+                val delta = (pageIndex - currentPageWithOffset).absoluteValue
+                val fraction = 1f - delta.coerceIn(0f, 1f)
+                SwatchMinScale + (SwatchMaxScale - SwatchMinScale) * fraction
+            }
+        }
+        Swatch(
+            modifier = Modifier
+                .zIndex(zIndex)
+                .scale(scale),
+            swatch = swatch,
+            onClick = { onSwatchClick(pageIndex) },
+        )
+    }
+}
+
+@Composable
+private fun calcContentPadding(pagerState: PagerState): State<PaddingValues?> {
+    val density = LocalDensity.current
+    return remember {
+        // PagerState.layoutInfo is a @FrequentlyChangingValue (not annotated but it is, just like in LazyListState)
+        derivedStateOf {
+            val viewportSize = pagerState.layoutInfo.viewportSize
+            val orientation = pagerState.layoutInfo.orientation
+            calcContentPadding(
+                density = density,
+                viewportSize = viewportSize,
+                orientation = orientation,
+            )
         }
     }
 }
+
+private fun calcContentPadding(
+    density: Density,
+    viewportSize: IntSize,
+    orientation: Orientation,
+): PaddingValues? {
+    if (viewportSize == IntSize.Zero) return null // undefined value in LayoutInfo implementations
+    val itemSize = with(density) { SwatchSize.toPx() }
+    val viewportSize = viewportSize.takeIf { it != IntSize.Zero } ?: return null
+    val viewportMainDimen = when (orientation) {
+        Orientation.Vertical -> viewportSize.height
+        Orientation.Horizontal -> viewportSize.width
+    }
+    val sizeWithoutCenterItem = (viewportMainDimen - itemSize)
+    val horizontalPadding = (sizeWithoutCenterItem / 2)
+    return PaddingValues(horizontal = with(density) { horizontalPadding.toDp() })
+}
+
+private fun Modifier.drawIf(shouldDraw: Boolean): Modifier =
+    this.drawWithContent {
+        if (shouldDraw) drawContent()
+    }
+
+// TODO: remove me
+private fun Modifier.drawCenterItemPointer(): Modifier =
+    this.drawBehind {
+        val itemRadius = SwatchSize / 2
+        val strokeWidth = 2.dp
+        val pointerSpacing = 1.dp
+        val pointerRadius = (itemRadius + pointerSpacing + strokeWidth)
+        drawCircle(
+            color = Color.Red,
+            radius = with(density) { pointerRadius.toPx() },
+            center = this.size.center,
+            style = Stroke(width = with(density) { strokeWidth.toPx() }),
+        )
+    }
 
 @Composable
 private fun Swatch(
     swatch: ColorSchemeData.Swatch,
     onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    colorOverride: Color? = null, // TODO: debug override, remove
 ) {
     val colors = rememberContentColors(useLight = swatch.isDark) // light content on dark and vice versa
     ProvideColorsOnTintedSurface(colors) { // provides correct ripple
         Box(
-            modifier = Modifier
-                .size(64.dp)
+            modifier = modifier
+                .size(SwatchSize)
                 .clip(CircleShape)
-                .background(swatch.color.toCompose())
+                .background(colorOverride ?: swatch.color.toCompose())
                 .clickable(onClick = onClick),
         )
     }
+}
+
+private val SwatchSize = 80.dp
+private val SwatchMinScale = 0.8f
+private val SwatchMaxScale = 1f
+private val SwatchSpacing = run {
+    val spaceBetweenAdjacentMinScaledSwatches = SwatchSize * (SwatchMaxScale - SwatchMinScale)
+    val halfOfMinScaledSwatchOffset = (SwatchSize * SwatchMinScale) / 2f
+    -(spaceBetweenAdjacentMinScaledSwatches + halfOfMinScaledSwatchOffset)
 }
 
 private fun Modifier.edgeToEdge(
