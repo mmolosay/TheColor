c1e696f722e4033f73c296084771da5768abea2f
mmolosay <mmolosays@gmail.com>
2025-11-16 02:14:25 +0100

    Add Composable Preview

--- presentation/acknowledgements/src/main/java/io/github/mmolosay/thecolor/presentation/acknowledgements/AcknowledgementsScreen.kt
+++ presentation/acknowledgements/src/main/java/io/github/mmolosay/thecolor/presentation/acknowledgements/AcknowledgementsScreen.kt
@@ -1,5 +1,6 @@
 package io.github.mmolosay.thecolor.presentation.acknowledgements
 
+import android.content.res.Configuration
 import android.widget.Toast
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,21 +32,29 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.fromHtml
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mikepenz.aboutlibraries.Libs
+import com.mikepenz.aboutlibraries.entity.Developer
 import com.mikepenz.aboutlibraries.entity.Library
+import com.mikepenz.aboutlibraries.entity.License
+import com.mikepenz.aboutlibraries.entity.Scm
 import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
-import com.mikepenz.aboutlibraries.ui.compose.android.produceLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import com.mikepenz.aboutlibraries.ui.compose.util.htmlReadyLicenseContent
 import io.github.mmolosay.debounce.debounced
+import io.github.mmolosay.thecolor.presentation.design.TheColorTheme
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.persistentSetOf
 import kotlin.time.Duration.Companion.milliseconds
+import com.mikepenz.aboutlibraries.ui.compose.android.produceLibraries as produceLibrariesByDefault
 import io.github.mmolosay.thecolor.presentation.design.R as DesignR
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AcknowledgementsScreen(
     navigateBack: () -> Unit,
+    produceLibraries: @Composable () -> State<Libs?> = { produceLibrariesByDefault() },
 ) {
     val context = LocalContext.current
     val libraries by produceLibraries()
@@ -192,5 +202,70 @@ private fun Author(
         color = MaterialTheme.colorScheme.onSurfaceVariant,
         maxLines = 1,
         overflow = TextOverflow.Ellipsis,
+    )
+}
+
+@Preview(uiMode = Configuration.UI_MODE_TYPE_NORMAL)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
+@Composable
+private fun Preview() {
+    TheColorTheme {
+        AcknowledgementsScreen(
+            navigateBack = {},
+            produceLibraries = { remember { mutableStateOf(previewLibraries()) } },
+        )
+    }
+}
+
+@Suppress("SpellCheckingInspection")
+private fun previewLibraries(): Libs {
+    val apacheLicense = License(
+        name = "Apache License 2.0",
+        url = "https://spdx.org/licenses/Apache-2.0.html",
+        year = "",
+        spdxId = "Apache-2.0",
+        licenseContent = "<License content>",
+        hash = "Apache-2.0",
+    )
+    return Libs(
+        libraries = persistentListOf(
+            Library(
+                uniqueId = "io.github.mmolosay:debounce",
+                artifactVersion = "1.2.0",
+                name = "debounce",
+                description = "Debounce your lambdas.",
+                website = "https://github.com/mmolosay/debounce",
+                developers = persistentListOf(
+                    Developer(name = "Misha Malasai", organisationUrl = ""),
+                ),
+                organization = null,
+                scm = Scm(
+                    connection = "scm:git:git://github.com/mmolosay/debounce.git",
+                    developerConnection = "scm:git:ssh://github.com/mmolosay/debounce.git",
+                    url = "https://github.com/mmolosay/debounce",
+                ),
+                licenses = persistentSetOf(apacheLicense),
+                funding = persistentSetOf(),
+            ),
+            Library(
+                uniqueId = "org.jetbrains.kotlin:kotlin-stdlib",
+                artifactVersion = "2.2.20",
+                name = "Kotlin Stdlib",
+                description = "Kotlin Standard Library",
+                website = "https://kotlinlang.org/",
+                developers = persistentListOf(
+                    Developer(name = "Kotlin Team", organisationUrl = "https://www.jetbrains.com"),
+                ),
+                organization = null,
+                scm = Scm(
+                    connection = "scm:git:https://github.com/JetBrains/kotlin.git",
+                    developerConnection = "scm:git:https://github.com/JetBrains/kotlin.git",
+                    url = "https://github.com/JetBrains/kotlin",
+                ),
+                licenses = persistentSetOf(apacheLicense),
+                funding = persistentSetOf(),
+            ),
+        ),
+        licenses = persistentSetOf(apacheLicense),
     )
 }
