e131912659cd2d73bc17a6a7f48e986c52c4cad9
mmolosay <mmolosays@gmail.com>
2026-02-22 20:21:50 +0100

    Fix bug
    New fetched `ColorDetails` were added to the `cachedDetails` set, which was reordering them based on hash code of the elements. This was breaking the logic of getting the most recent cached `ColorDetails`.

--- presentation/color-details/src/main/java/io/github/mmolosay/thecolor/presentation/details/viewmodel/ColorDetailsViewModel.kt
+++ presentation/color-details/src/main/java/io/github/mmolosay/thecolor/presentation/details/viewmodel/ColorDetailsViewModel.kt
@@ -20,7 +20,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArraySet
 import java.util.concurrent.atomic.AtomicReference
 import javax.inject.Inject
 import javax.inject.Named
@@ -53,7 +53,7 @@ class ColorDetailsViewModel @AssistedInject constructor(
     val dataStateFlow = _dataStateFlow.asStateFlow()
 
     private val fetchOrFindColorDetailsJob = AtomicReference<Job?>(null)
-    private val cachedDetails = ConcurrentHashMap.newKeySet<DomainColorDetails>()
+    private val cachedDetails = CopyOnWriteArraySet<DomainColorDetails>()
 
     init {
         collectColorDetailsCommands()
