543c32ee5c1ad9e95da59efaa6701ae2d7922c27
mmolosay <mmolosays@gmail.com>
2026-02-25 13:26:02 +0100

    Replace `ImmediateEventRelay` with new `ConsumableStore`

--- presentation/color-input/src/test/java/io/github/mmolosay/thecolor/presentation/input/ColorInputHexViewModelTest.kt
+++ presentation/color-input/src/test/java/io/github/mmolosay/thecolor/presentation/input/ColorInputHexViewModelTest.kt
@@ -10,6 +10,7 @@ import io.github.mmolosay.thecolor.presentation.input.model.DataState
 import io.github.mmolosay.thecolor.presentation.input.textfield.TextFieldData.Text
 import io.github.mmolosay.thecolor.presentation.input.textfield.TextFieldViewModel
 import io.github.mmolosay.thecolor.testing.MainDispatcherExtension
+import io.github.mmolosay.thecolor.utils.pending
 import io.kotest.assertions.withClue
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
@@ -27,7 +28,6 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
@@ -192,7 +192,7 @@ class ColorInputHexViewModelTest {
             coVerify(exactly = 1) {
                 submitAction.invoke(colorInput = any(), validationResult = any())
             }
-            val submissionResult = sut.colorSubmissionResultFlow.first()
+            val submissionResult = sut.submissionResultStore.pending.last().value
             submissionResult.wasAccepted shouldBe true
         }
 
