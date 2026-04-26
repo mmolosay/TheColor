7c3f34688de077e16e4fabc92dbde5fbf4688962
mmolosay <mmolosays@gmail.com>
2026-04-27 00:04:16 +0200

    Extract `ColorCenterSessionStore` into an individual file

---
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/viewmodel/ColorCenterSessionStore.kt
@@ -0,0 +1,103 @@
+package io.github.mmolosay.thecolor.presentation.home.viewmodel
+
+import io.github.mmolosay.thecolor.domain.color.Color
+import io.github.mmolosay.thecolor.presentation.home.viewmodel.ColorCenterSessionStore.SessionState
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+/* 'internal' for testing, 'private' for HomeViewModel */
+internal class ColorCenterSessionStore {
+
+    private val _flowOfSessionState = MutableStateFlow<SessionState>(SessionState.NoSession)
+    val flowOfSessionState = _flowOfSessionState.asStateFlow()
+
+    private val updateStateMutex = Mutex()
+
+    suspend fun clear() =
+        updateStateMutex.withLock {
+            sessionState.cancelIfBuilding()
+            _flowOfSessionState.emit(SessionState.NoSession)
+        }
+
+    suspend fun startBuilding(seed: Color, job: Job): SessionBuildingScope =
+        updateStateMutex.withLock {
+            sessionState.cancelIfBuilding()
+            val newState = SessionState.BeingBuilt(seed, job)
+            _flowOfSessionState.emit(newState)
+            return SessionBuildingScopeImpl(origin = newState)
+        }
+
+    private fun SessionState.cancelIfBuilding() {
+        if (this is SessionState.BeingBuilt) {
+            this.job.cancel()
+        }
+    }
+
+    interface SessionBuildingScope {
+
+        /**
+         * Completes the building process of this scope.
+         * Emits [SessionState.Ongoing] containing [session] from the [flowOfSessionState].
+         *
+         * This method is cancellation-cooperative, meaning that if the [SessionState.BeingBuilt.job]
+         * is already canceled, then the method will re-throw original [kotlin.coroutines.cancellation.CancellationException].
+         *
+         * @return `true` if the process was completed successfully, `false` if the [sessionState]
+         * has changed already and the scope is "stale".
+         */
+        suspend fun complete(session: ColorCenterSession): Boolean
+
+        /**
+         * Cancels the building process of this scope.
+         * Emits [SessionState.NoSession] from the [flowOfSessionState].
+         *
+         * @return `true` if the process was canceled successfully, `false` if the [sessionState]
+         * has changed already and the scope is "stale".
+         */
+        suspend fun cancel(): Boolean
+    }
+
+    private inner class SessionBuildingScopeImpl(
+        private val origin: SessionState.BeingBuilt,
+    ) : SessionBuildingScope {
+
+        override suspend fun complete(session: ColorCenterSession): Boolean {
+            origin.job.ensureActive()
+            updateStateMutex.withLock {
+                if (sessionState == origin) {
+                    _flowOfSessionState.emit(SessionState.Ongoing(session))
+                    return true
+                }
+            }
+            return false
+        }
+
+        override suspend fun cancel(): Boolean {
+            updateStateMutex.withLock {
+                if (sessionState == origin) {
+                    origin.job.cancel()
+                    _flowOfSessionState.emit(SessionState.NoSession)
+                    return true
+                }
+            }
+            return false
+        }
+    }
+
+    sealed interface SessionState {
+        data object NoSession : SessionState
+        data class BeingBuilt(val seed: Color, val job: Job) : SessionState
+        data class Ongoing(val session: ColorCenterSession) : SessionState
+    }
+}
+
+internal val ColorCenterSessionStore.sessionState: SessionState
+    get() = this.flowOfSessionState.value
+
+internal fun SessionState.mustBeOngoing() {
+    check(this is SessionState.Ongoing) { "SessionState $this must be Ongoing" }
+}
--- presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/viewmodel/HomeViewModel.kt
+++ presentation/home/src/main/java/io/github/mmolosay/thecolor/presentation/home/viewmodel/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package io.github.mmolosay.thecolor.presentation.home.viewmodel
 
-import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -41,8 +40,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -52,11 +49,8 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.coroutines.cancellation.CancellationException
 import io.github.mmolosay.thecolor.domain.color.ColorDetails as DomainColorDetails
 
 /**
@@ -460,100 +454,6 @@ class HomeViewModel @Inject constructor(
         data object Proceed : Operation
         data object ConsumeColorCenterComponents : Operation
     }
-}
-
-/* 'internal' for testing, 'private' in file */
-@VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-internal class ColorCenterSessionStore {
-
-    private val _flowOfSessionState = MutableStateFlow<SessionState>(SessionState.NoSession)
-    val flowOfSessionState = _flowOfSessionState.asStateFlow()
-
-    private val updateStateMutex = Mutex()
-
-    suspend fun clear() =
-        updateStateMutex.withLock {
-            sessionState.cancelIfBuilding()
-            _flowOfSessionState.emit(SessionState.NoSession)
-        }
-
-    suspend fun startBuilding(seed: Color, job: Job): SessionBuildingScope =
-        updateStateMutex.withLock {
-            sessionState.cancelIfBuilding()
-            val newState = SessionState.BeingBuilt(seed, job)
-            _flowOfSessionState.emit(newState)
-            return SessionBuildingScopeImpl(origin = newState)
-        }
-
-    private fun SessionState.cancelIfBuilding() {
-        if (this is SessionState.BeingBuilt) {
-            this.job.cancel()
-        }
-    }
-
-    interface SessionBuildingScope {
-
-        /**
-         * Completes the building process of this scope.
-         * Emits [SessionState.Ongoing] containing [session] from the [flowOfSessionState].
-         *
-         * This method is cancellation-cooperative, meaning that if the [SessionState.BeingBuilt.job]
-         * is already canceled, then the method will re-throw original [CancellationException].
-         *
-         * @return `true` if the process was completed successfully, `false` if the [sessionState]
-         * has changed already and the scope is "stale".
-         */
-        suspend fun complete(session: ColorCenterSession): Boolean
-
-        /**
-         * Cancels the building process of this scope.
-         * Emits [SessionState.NoSession] from the [flowOfSessionState].
-         *
-         * @return `true` if the process was canceled successfully, `false` if the [sessionState]
-         * has changed already and the scope is "stale".
-         */
-        suspend fun cancel(): Boolean
-    }
-
-    private inner class SessionBuildingScopeImpl(
-        private val origin: SessionState.BeingBuilt,
-    ) : SessionBuildingScope {
-
-        override suspend fun complete(session: ColorCenterSession): Boolean {
-            origin.job.ensureActive()
-            updateStateMutex.withLock {
-                if (sessionState == origin) {
-                    _flowOfSessionState.emit(SessionState.Ongoing(session))
-                    return true
-                }
-            }
-            return false
-        }
-
-        override suspend fun cancel(): Boolean {
-            updateStateMutex.withLock {
-                if (sessionState == origin) {
-                    sessionState.cancelIfBuilding()
-                    _flowOfSessionState.emit(SessionState.NoSession)
-                    return true
-                }
-            }
-            return false
-        }
-    }
-
-    sealed interface SessionState {
-        data object NoSession : SessionState
-        data class BeingBuilt(val seed: Color, val job: Job) : SessionState
-        data class Ongoing(val session: ColorCenterSession) : SessionState
-    }
-}
-
-private val ColorCenterSessionStore.sessionState: SessionState
-    get() = this.flowOfSessionState.value
-
-private fun SessionState.mustBeOngoing() {
-    check(this is SessionState.Ongoing) { "SessionState $this must be Ongoing" }
 }
 
 /**
