fb06417cf5d43cca4d56b73045658869ce28e2d5
mmolosay <mmolosays@gmail.com>
2026-06-16 17:11:06 +0200

    Replace `NoOpActionWithResult()` with a flat `ActionWithResult()` factory

--- utils/src/main/java/io/github/mmolosay/thecolor/utils/ActionWithResult.kt
+++ utils/src/main/java/io/github/mmolosay/thecolor/utils/ActionWithResult.kt
@@ -1,11 +1,26 @@
 package io.github.mmolosay.thecolor.utils
 
-data class ActionWithResult<R>(
+data class ActionWithResult<R : Any>(
     val result: AckValue<R>? = null,
     val action: () -> Unit, // placed last to enable trailing lambda syntax
 )
 
-fun <R> NoOpActionWithResult(): ActionWithResult<R> =
+fun <R : Any> ActionWithResult(
+    result: R?,
+    resultAck: () -> Unit,
+    action: () -> Unit,
+): ActionWithResult<R> =
+    ActionWithResult(
+        result = result?.let {
+            AckValue(
+                value = it,
+                ack = resultAck,
+            )
+        },
+        action = action,
+    )
+
+fun <R : Any> NoOpActionWithResult(): ActionWithResult<R> =
     ActionWithResult(
         result = null,
         action = ::doNothing,
