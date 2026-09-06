a42b6f89e466027352d5fb97b7eac9e7a98605f4
mmolosay <mmolosays@gmail.com>
2026-09-07 00:16:02 +0200

    Extract `Lens` into an individual file

--- utils/src/main/java/io/github/mmolosay/thecolor/utils/Store.kt
+++ utils/src/main/java/io/github/mmolosay/thecolor/utils/Store.kt
@@ -242,17 +242,3 @@ private class StoreView<Source, T>(
         }
     }
 }
-
-interface Lens<Source, Value> {
-    fun get(source: Source): Value
-    fun set(source: Source, value: Value): Source
-}
-
-fun <S, V> Lens(
-    get: (source: S) -> V,
-    set: (source: S, value: V) -> S,
-): Lens<S, V> =
-    object : Lens<S, V> {
-        override fun get(source: S): V = get(source)
-        override fun set(source: S, value: V): S = set(source, value)
-    }
